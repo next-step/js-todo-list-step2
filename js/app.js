bb2887c217f30199394ab4313e1a1dc1a9c7b970
Parkjoopika <parkjoohan0603@gmail.com>
2020-06-27 00:47:49 +0900

    심화 요구사항을 제외한 나머지 구현

--- js/app.js
+++ js/app.js
@@ -5,39 +5,155 @@ const filt_ul = document.querySelector(".filters")
 const all_a = filt_ul.querySelector(".all")
 const active_a = filt_ul.querySelector(".active")
 const complete_a = filt_ul.querySelector(".completed")
+const userDiv = document.querySelector("#user-list") 
+const userInput = document.querySelector(".adduser")
+const userH1 = document.querySelector("#user-title")
 const ENTER = "Enter"
 const ESC = "Escape"
 const ALL = "all"
 const ACTIVE = 'active'
 const COMPLETED = 'completed'
-const APIURL = 'https://blackcoffee-todolist.df.r.appspot.com/api/u/parkchu/item'
+const APIURL = 'https://blackcoffee-todolist.df.r.appspot.com/api/u/'
+let userList = []
 let todo_list = []
 let id = 0
+let userId = 0
+let currentUser = "guest"
 
-async function apiLoadTodo() {
-    todo_list = await fetch(APIURL).then(function(request){
-        return request.json()
-    }).then(function(json){
-        setTimeout(() => {
-        })
-        return json.todoList
+function apiCheck(todoId) {
+    fetch(APIURL + `${currentUser}/item/${todoId}/toggle/`, {
+        method: "PUT"
     })
-    todo_list.forEach(function(todo){
-        drawTodo(todo)
+}
+
+function apiEdit(todoId, editTodo) {
+    fetch(APIURL + `${currentUser}/item/${todoId}/`, {
+        method: "PUT",
+        headers: {
+            'content-Type': 'application/json; charset=UTF-8'
+        },
+        body: JSON.stringify({
+            contents: editTodo
+        })
+    })
+}
+
+function apiDelTodo(todoId) {
+    fetch(APIURL + `${currentUser}/item/${todoId}`, {
+        method: "DELETE"
     })
     countTodo()
 }
 
-function apiSaveTodo(todo) {
-    fetch(APIURL, {
-        method: "POST",
-        headers: {
-            'content-type': 'application/json; charset=UTF-8'
-        },
-        body: JSON.stringify({
-            contents: todo.contents
-        })
+async function apiLoadUser() {
+    const user = await fetch(APIURL + `${currentUser}/item/`).then(function(res){
+        return res.json()
+    }).then(function(user){
+        return user
     })
+    todo_list = user.todoList
+    loadTodo()
+    countTodo()
+} 
+
+function loadTodo() {
+    todo_list.forEach(function(todo){
+        drawTodo(todo)
+    })
+}
+
+function findUser(btn) {
+    return userList.find(user => user._id == btn.id)
+}
+
+function clickUser(event){
+    const btn = event.target
+    const user = findUser(btn)
+    currentUser = user.name
+    delActiveUser()
+    allClear()
+    apiLoadUser()
+    btn.classList.add("active")
+    userH1.innerHTML = `<span><strong>${currentUser}</strong>'s Todo List</span>`
+}
+
+function makeUserObj(userName){
+    const user = {
+        "_id": id,
+        'name': userName 
+    }
+    userId = userId + 1
+    userList.push(user)
+    drawUser(user)
+}
+
+function addNewUser(event) {
+    if (event.key === ENTER) {
+        valueUser = event.target.value
+        event.target.value = ""
+        fetch(APIURL + `${valueUser}/item/`, {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json; charset=UTF-8'
+            },
+            body: JSON.stringify({
+                contents: "새로운 유저가 되신것을 환영합니다~"
+            })
+        })
+        makeUserObj(valueUser)
+    }
+}
+
+function delActiveUser() {
+    const userBtnList = userDiv.querySelectorAll('*')
+    userBtnList.forEach(function(user){
+        user.classList.remove("active")
+    })
+}
+
+function addUser(){
+    userList.forEach(function(user){
+        drawUser(user)
+    })
+}
+
+function drawUser(user) {
+    const btn = document.createElement("button")
+    btn.classList.add("ripple")
+    btn.id = user._id
+    btn.innerText = user.name
+    btn.addEventListener("click", clickUser)
+    userDiv.appendChild(btn)
+}
+
+async function apiLoadUserList() {
+    userList = await fetch(APIURL)
+    .then(function(res){
+        return res.json()
+    }).then(function(user){
+        return user
+    })
+    addUser()
+}
+
+async function apiSaveTodo(todo) {
+    if (!(currentUser === "guest")) {
+        const apiTodo = await fetch(APIURL + `${currentUser}/item/`, {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json; charset=UTF-8'
+            },
+            body: JSON.stringify({
+                contents: todo
+            })
+        }).then(function(res){
+            return res.json()
+        }).then(function(todo){
+            return todo
+        })
+        todo_list.push(apiTodo)
+        return apiTodo
+    }
 }
 
 function allClear() {
@@ -62,10 +178,10 @@ function filterTodo(click_btn) {
             return true
         } else if (click_btn === ACTIVE) {
             active_a.classList.add("selected")
-            return !todo.complete
+            return !todo.isCompleted
         } else if (click_btn === COMPLETED) {
             complete_a.classList.add("selected")
-            return todo.complete
+            return todo.isCompleted
         }
     })
     return filter_todo
@@ -82,8 +198,7 @@ function deleteTodo(event) {
         const todo = findTodo(li)
         todo_list.splice(todo_list.indexOf(todo), 1)
         todo_ul.removeChild(li)
-        countTodo()
-        localSave()
+        apiDelTodo(todo._id)
     }
 }
 
@@ -99,8 +214,8 @@ function editingTodo(event) {
                 todo = findTodo(li)
                 label.innerText = event.target.value
                 li.classList.remove("editing")
-                todo.todo = label.innerText
-                localSave()
+                todo.contents = label.innerText
+                apiEdit(todo._id, todo.contents)
             }
         }
     }
@@ -123,19 +238,19 @@ function todoComplete(event) {
         todo = findTodo(li)
         if (event.target.checked) {
             li.classList.add("completed")
-            todo.complete = true
+            todo.isCompleted = true
         } else {
             li.classList.remove("completed")
-            todo.complete = false
+            todo.isCompleted = false
         }
-        localSave()
+        apiCheck(todo._id)
     }
 }
 
 function makeTodoObj(current_todo, boolean) {
     todo = {
         'contents': current_todo,
-        'complete': boolean,
+        "isCompleted": boolean,
         '_id': id
     }
     id = id + 1
@@ -147,15 +262,20 @@ function checkBlank(input_value){
     return !input_value
 }
 
-function inputTodo(event) {
+async function inputTodo(event) {
     if (event.key === ENTER) {
         current_todo = todo_input.value
         todo_input.value = ""
         if (!checkBlank(current_todo)) {
-            const todo = makeTodoObj(current_todo, false)
-            drawTodo(todo)
-            countTodo()
-            apiSaveTodo(todo)
+            if (currentUser === "guest") {
+                const todo = makeTodoObj(current_todo, false)
+                drawTodo(todo)
+                countTodo()
+            } else {
+                const todo = await apiSaveTodo(current_todo)
+                drawTodo(todo)
+                countTodo()
+            }
         }
     }
 }
@@ -169,11 +289,11 @@ function drawTodo(todo) {
                 <label class="label">${todo.contents}</label>
                 <button class="delete"></button>
             </div>
-            <input class="edit" value="${todo.todo}" />
+            <input class="edit" value="${todo.contents}" />
         </li>
         `
     }
-    if (todo.complete) {
+    if (todo.isCompleted) {
         const todoItem = todoItemTemplate(todo, "completed", "checked")
         todo_ul.innerHTML += todoItem
     } else {
@@ -191,7 +311,9 @@ function init() {
     todo_ul.addEventListener("keyup", editingTodo)
     todo_ul.addEventListener("dblclick", editTodo)
     todo_ul.addEventListener("click", deleteTodo)
-    apiLoadTodo()
+    userInput.addEventListener("keyup", addNewUser)
+    apiLoadUserList()
+   
 }
 
 init()
