717caf00725f56f9043cfbce3f79f1a19a777270
2SOOY <1401660@naver.com>
2020-08-17 02:02:36 +0900

    Add - constant.js : DOM selector
    - DOM 객체에 대한 css 선택자 상수로 관리

---
+++ js/utils/constant.js
@@ -0,0 +1,10 @@
+export const SELECTOR = {
+  TODO_APP: '#app',
+  TODO_TITLE: '#user-title',
+  USER_LIST: '#user-list',
+  TODO_INPUT: '.todo-input',
+  TODO_LIST: '.todo-list',
+  TODO_COUNT: '.todo-count',
+  TODO_TAB: 'todo-tab',
+  TODO_CLEAR_BUTTON: '.clear-all',
+};
