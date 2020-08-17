bcbb59a70ef6245066bc37af6a1887c7473db52e
2SOOY <1401660@naver.com>
2020-08-17 16:23:44 +0900

    Add - TodoTab component in App
    - index.html내 tab의 className 추가 all, active, completed
    - TodoTab 컴포넌트를 위한 onChangeTab 메소드 추가
    - TodoTab에서 클릭 이벤트 발생 시 App state의 selectedTab 변경

--- js/Components/App.js
+++ js/Components/App.js
@@ -3,8 +3,9 @@ import UserList from './UserList.js';
 import TodoList from './TodoList.js';
 import TodoInput from './TodoInput.js';
 import TodoCount from './TodoCount.js';
+import TodoTab from './TodoTab.js';
 
-import { SELECTOR } from '../utils/constant.js';
+import { SELECTOR, CLASS_NAME } from '../utils/constant.js';
 import { api } from '../utils/api.js';
 
 function App($target) {
@@ -16,7 +17,7 @@ function App($target) {
         todos: [],
       },
       users: [],
-      selectedTab: 'all',
+      selectedTab: CLASS_NAME.ALL,
     };
 
     this.state.users = await api.fetchUserList();
@@ -60,14 +61,25 @@ function App($target) {
         selectedTab: this.state.selectedTab,
       },
     });
+
+    this.todoTab = new TodoTab({
+      $target: document.querySelector(SELECTOR.TODO_TAB),
+      todoTabState: {
+        userName: this.state.user.name,
+        selectedTab: this.state.selectedTab,
+      },
+      onChangeTab: this.onChangeTab,
+    });
   };
 
+  // UserList
   this.onChangeUser = async (userName) => {
     const newState = await this.fetchState(userName);
 
     this.setState(newState);
   };
 
+  // TodoList
   this.onAddTodo = async (userName, contents) => {
     await api.addTodo(userName, contents);
     const newState = await this.fetchState(userName);
@@ -96,6 +108,17 @@ function App($target) {
     this.setState(newState);
   };
 
+  // TodoTab
+  this.onChangeTab = (selectedTab) => {
+    const newState = {
+      ...this.state,
+      selectedTab,
+    };
+
+    this.setState(newState);
+  };
+
+  // 공통
   this.fetchState = async (userName) => {
     const users = await api.fetchUserList();
     const userInfo = await api.fetchUserTodo(userName);
@@ -131,6 +154,7 @@ function App($target) {
       todos: this.state.user.todos,
       selectedTab: this.state.selectedTab,
     });
+    this.todoTab.setState(this.state.selectedTab);
   };
 
   this.init();
