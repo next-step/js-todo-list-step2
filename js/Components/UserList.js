5155d49aad18ba0a79d11a26ce66bbcb00edee3b
lsw1164 <qkqkfo@gmail.com>
2020-08-18 02:25:09 +0900

    Add Loader Component, Fetch UserList with Loader

--- js/Components/UserList.js
+++ js/Components/UserList.js
@@ -1,5 +1,6 @@
 import { fetchTodoUsersFromServer } from "../api.js";
 import { validateUserName, isFunction } from "../utils.js";
+import Loader from "../Components/Loader.js";
 
 function UserList($target, activeUser, eventHandler) {
   if (!new.target) {
@@ -12,6 +13,7 @@ function UserList($target, activeUser, eventHandler) {
 
   validateUserName(activeUser);
   this.activeUser = activeUser;
+  this.users = [];
 
   this.setState = (newActiveUser) => {
     validateUserName(newActiveUser);
@@ -19,16 +21,18 @@ function UserList($target, activeUser, eventHandler) {
     this.render();
   };
 
-  this.render = async () => {
-    const users = await fetchTodoUsersFromServer();
-    $target.innerHTML = users
-      .map(
-        ({ name }) =>
-          `<button class="ripple ${
-            this.activeUser === name ? "active" : ""
-          }">${name}</button>`
-      )
-      .join(" ");
+  this.render = () => {
+    $target.innerHTML = `
+        <div class="loader"></div>
+        ${this.users
+          .map(
+            ({ name }) =>
+              `<button class="ripple ${
+                this.activeUser === name ? "active" : ""
+              }">${name}</button>`
+          )
+          .join(" ")}
+        `;
   };
 
   this.bindEvent = () => {
@@ -39,8 +43,17 @@ function UserList($target, activeUser, eventHandler) {
     });
   };
 
-  this.render();
+  this.fetchUserListWithLoader = async () => {
+    this.loader = new Loader($target.querySelector(".loader"));
+    this.loader.setState(true);
+    this.users = await fetchTodoUsersFromServer();
+    this.loader.setState(false);
+    this.render();
+  };
+
   this.bindEvent();
+  this.render();
+  this.fetchUserListWithLoader();
 }
 
 export default UserList;
