5372bf47922a7ed22ebc2fa827d24e9d52ca7204
EastjunDev <eastjun.dev@gmail.com>
2020-08-18 20:52:29 +0900

    refactor(api): add try catch to control exception

--- js/api.js
+++ js/api.js
@@ -1,10 +1,15 @@
 export const fetchUserListFromServer = async () => {
-  const res = await fetch(
-    "https://blackcoffee-todolist.df.r.appspot.com/api/u",
-    {
-      method: "GET",
-    }
-  );
-  const userList = await res.json();
-  return userList;
+  try {
+    const res = await fetch(
+      "https://blackcoffee-todolist.df.r.appspot.com/api/u",
+      {
+        method: "GET",
+      }
+    );
+    const userList = await res.json();
+    return userList;
+  } catch (e) {
+    return []
+    console.error(e)
+  }
 };
