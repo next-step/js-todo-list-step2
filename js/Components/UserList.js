4edce86d4bd7b34323596075d11cb0754fcdaf72
EastjunDev <eastjun.dev@gmail.com>
2020-08-18 20:52:44 +0900

    feat(userlist): add eventListeners

--- js/Components/UserList.js
+++ js/Components/UserList.js
@@ -13,15 +13,31 @@ function UserList($target, activeUser) {
   this.render = () => {
     this.$target.innerHTML = this.userList.map(
       ({ name }) =>
-        `<button class="ripple ${
+        `<button class="ripple user-button ${
           this.activeUser === name ? "active" : ""
         }"> ${name} </button>`
-    );
+    ).join('');
   };
 
+  this.initEventListeners = () => {
+    this.$target.addEventListener('click', this.onUserClickHandler.bind(this))
+  }
+
+  this.onUserClickHandler = (e) => {
+    if (!e.target.classList.contains('user-button')) {
+      return
+    }
+  }
+
   this.init = async () => {
-    this.userList = await fetchUserListFromServer();
-    this.render();
+    try {
+      this.userList = await fetchUserListFromServer();
+      this.render();
+      this.initEventListeners()
+    } catch (e) {
+      alert('유저리스트를 불러오는데 에러가 발생했습니다.')
+      console.error(e)
+    }
   };
 
   this.init();
