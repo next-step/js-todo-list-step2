97a4383c06a500fbb71bd3fab650e0576379e924
lsw1164 <qkqkfo@gmail.com>
2020-08-26 20:39:16 +0900

    Remove Dummy Loader in TodoList

--- js/Components/TodoList.js
+++ js/Components/TodoList.js
@@ -126,17 +126,6 @@ function TodoList($target, todoItems, eventHandler) {
 
     $target.innerHTML = `
         <ul class="todo-list">
-            <li>
-              <div class="view">
-                <label class="label">
-                  <div class="animated-background">
-                    <div class="skel-mask-container">
-                      <div class="skel-mask"></div>
-                    </div>
-                  </div>
-                </label>
-              </div>
-            </li>
             ${todoItemsHTML}
         </ul>
     `;
