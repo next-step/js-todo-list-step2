e1c8218895de7189f31239d0f014069e09488390
junilHwang <tjsdlf4261@naver.com>
2020-09-05 23:42:53 +0900

    feat: user별 투두리스트 불러오기 "완료"

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,19 +1,32 @@
 import { Component } from "../core/Component.js";
-import { userStore } from "../store/userStore.js";
+import { SET_USER, userStore } from "../store/userStore.js";
+import { FETCH_ITEMS, SET_LOADING_TYPE, todoStore } from "../store/todoStore.js";
+import LoadingTypes from "../constants/LoadingTypes.js";
+import { lazyFrame } from "../utils/index.js";
 
 export const UserList = class extends Component {
 
   render () {
     const { users, selectedIndex } = userStore.$state;
-    return users.map(({ _id, name }, index) => `
-      <button class="ripple ${index === selectedIndex}" data-id="${_id}">${name}</button>
+    return users.map(({ name }, index) => `
+      <button class="ripple ${index === selectedIndex ? 'active' : ''}" data-index="${index}">${name}</button>
     `).join('');
   }
 
   setEvent (componentTarget) {
     componentTarget.addEventListener('click', ({ target }) => {
       if (!target.classList.contains('ripple')) return;
-
+      this.#loadItemsByUser(Number(target.dataset.index));
     })
+  }
+
+  async #loadItemsByUser (index) {
+    userStore.commit(SET_USER, index);
+    todoStore.commit(SET_LOADING_TYPE, LoadingTypes.INIT);
+    await Promise.all([
+      todoStore.dispatch(FETCH_ITEMS, userStore.$getters.selectedUser.name),
+      lazyFrame(),
+    ])
+    todoStore.commit(SET_LOADING_TYPE, LoadingTypes.LOADED);
   }
 }
