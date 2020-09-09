852ce418a38f041f03eab7b82e65469860ee1ca1
junilHwang <tjsdlf4261@naver.com>
2020-09-09 19:12:23 +0900

    feat: TodoService 작업 완료

--- src/services/TodoService.js
+++ src/services/TodoService.js
@@ -1,55 +1,49 @@
 import HttpMethod from '../constants/HttpMethod.js';
-
-const API_PATH = 'https://blackcoffee-todolist.df.r.appspot.com/api/u';
-const headers = { 'Content-Type': 'application/json' };
+import {todoAdapter} from "../adapter/todoAdapter.js";
 
 export default Object.freeze({
 
   fetchUsers () {
-    return fetch(API_PATH).then(response => response.json());
+    return todoAdapter.get('/users');
   },
 
-  fetchItems (user) {
-    const path = `${API_PATH}/${user}/item`;
-    return fetch(path).then(response => response.json());
+  fetchUser (userId) {
+    return todoAdapter.get(`/users/${userId}`)
   },
 
-  addItem (user, contents) {
-    const path = `${API_PATH}/${user}/item`;
-    const body = JSON.stringify({ contents });
-    const option = { method: HttpMethod.POST, headers, body };
-    return fetch(path, option).then(response => response.json());
+  addUser (name) {
+    return todoAdapter.post('/users', { name });
   },
 
-  putItem (user, { _id, contents }) {
-    const path = `${API_PATH}/${user}/item/${_id}`;
-    const body = JSON.stringify({ contents });
-    const option = { method: HttpMethod.PUT, headers, body };
-    return fetch(path, option).then(response => response.json());
+  removeUser (userId) {
+    return todoAdapter.delete(`/users/${userId}`);
   },
 
-  toggleItem (user, id) {
-    const path = `${API_PATH}/${user}/item/${id}/toggle`;
-    const option = { method: HttpMethod.PUT, headers };
-    return fetch(path, option).then(response => response.json());
+  fetchItems (userId) {
+    return todoAdapter.get(`/users/${userId}/items`);
   },
 
-  removeItem (user, id) {
-    const path = `${API_PATH}/${user}/item/${id}`;
-    const option = { method: HttpMethod.DELETE };
-    return fetch(path, option).then(response => response.json());
+  addItem (userId, contents) {
+    return todoAdapter.get(`/users/${userId}/items`, { contents });
   },
 
-  removeAllItem (user) {
-    const path = `${API_PATH}/${user}/items`;
-    const option = { method: HttpMethod.DELETE };
-    return fetch(path, option).then(response => response.json());
+  putItem (userId, { _id: itemId, contents }) {
+    return todoAdapter.put(`/users/${userId}/items/${itemId}`, { contents });
   },
 
-  putPriorityItem (user, { _id, priority }) {
-    const path = `${API_PATH}/${user}/item/${_id}/priority`;
-    const body = JSON.stringify({ priority });
-    const option = { method: HttpMethod.PUT, headers, body };
-    return fetch(path, option).then(response => response.json());
+  toggleItem (userId, itemId) {
+    return todoAdapter.put(`/users/${userId}/items/${itemId}/toggle`);
+  },
+
+  putPriorityItem (userId, { _id: itemId, priority }) {
+    return todoAdapter.put(`/users/${userId}/items/${itemId}/priority`, { priority });
+  },
+
+  removeItem (userId, itemId) {
+    return todoAdapter.delete(`/users/${userId}/items/${itemId}`);
+  },
+
+  removeAllItem (userId) {
+    return todoAdapter.delete(`/users/${userId}/items`);
   },
 })
