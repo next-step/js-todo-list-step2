23c57c2a4e6196cf7d6ab3473ec7747803d976a1
junilHwang <tjsdlf4261@naver.com>
2020-09-10 20:02:52 +0900

    feat: RestClient 작성 완료

---
+++ src/constants/HttpMethod.js
@@ -0,0 +1,7 @@
+export const GET = 'GET';
+export const POST = 'POST';
+export const PUT = 'PUT';
+export const PATCH = 'PATCH';
+export const DELETE = 'DELETE';
+
+export default { GET, POST, PUT, PATCH, DELETE };
---
+++ src/core/RestClient.js
@@ -0,0 +1,51 @@
+import HttpMethod from '../constants/HttpMethod.js';
+
+export const RestClient = class {
+
+  #baseURL;
+
+  constructor (baseURL) {
+    this.#baseURL = baseURL;
+  }
+
+  #getUrlOf (uri) {
+    return `${this.#baseURL}/${uri}`;
+  }
+
+  #request (uri, method) {
+    const url = this.#getUrlOf(uri);
+    return fetch(url, { method }).then(response => response.json());
+  }
+
+  #requestWithBody (uri, method, body = null) {
+    const url = this.#getUrlOf(uri);
+    const headers =  { 'Content-Type': 'application/json' };
+    const config = { method, headers };
+    if (body !== null) {
+      config.body = JSON.stringify(body);
+    }
+    return fetch(url, config).then(response => response.json());
+  }
+
+  get (uri) {
+    return this.#request(uri, HttpMethod.GET);
+  }
+
+  post (uri, body) {
+    return this.#requestWithBody(uri, HttpMethod.POST, body);
+  }
+
+  put (uri, body) {
+    return this.#requestWithBody(uri, HttpMethod.PUT, body);
+
+  }
+
+  patch (uri, body) {
+    return this.#requestWithBody(uri, HttpMethod.PATCH, body);
+  }
+
+  delete (uri) {
+    return this.#request(uri, HttpMethod.DELETE);
+  }
+
+}
