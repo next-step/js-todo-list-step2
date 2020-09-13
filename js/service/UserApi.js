152a434c64bdc5ee54fe540b9b320bdf0e9c4768
wani <oen142@naver.com>
2020-09-13 15:53:49 +0900

    feat : create UserAPI

---
+++ js/service/UserApi.js
@@ -0,0 +1,39 @@
+import {request , options} from "./core.js";
+import {API_URL} from "../utils/constants";
+
+
+const userApi = {
+    getUserList : () => {
+        return request(`${API_URL}/api/users/`);
+    },
+    postUser : (contents) => {
+        return request(`${API_URL}/api/users/` ,options.POST(contents) );
+    },
+    getUser : (userId) =>{
+        return request(`${API_URL}/api/users/${userId}/` );
+    },
+    deleteUser : (userId) => {
+        return request(`${API_URL}/api/users/${userId}/` , options.DELETE());
+    },
+    getUserTodoItem : (userId) =>{
+        return request(`${API_URL}/api/users/${userId}/items/`);
+    },
+    postUserTodoItem : (userId , contents) =>{
+        return request(`${API_URL}/api/users/${userId}/items/` , options.POST(contents));
+    },
+    deleteUserAllTodoItem : (userId ) =>{
+        return request(`${API_URL}/api/users/${userId}/items/` , options.DELETE());
+    },
+    deleteUserOneTodoItem : (userId , todoItem) => {
+        return request(`${API_URL}/api/users/${userId}/items/${todoItem}` , options.DELETE());
+    },
+    putUserTodoItem : (userId, todoItem) => {
+        return request(`${API_URL}/api/users/${userId}/items/${todoItem}` , options.PUT());
+    },
+    putUserTodoItemPriority : (userId , todoItem , priority) => {
+        return request(`${API_URL}/api/users/${userId}/items/${todoItem}/priority` , options.PUT_PRIORITY(priority));
+    },
+    putUserTodoItemCompleteToggle : (userId , todoItem) => {
+        return request(`${API_URL}/api/users/${userId}/items/${todoItem}/toggle` , options.PUT_TOGGLE());
+    }
+}
