ab454e0c944a4603bb99364ead9d85cba2b320b2
jho2301 <jho2301@gmail.com>
2020-09-13 18:36:57 +0900

    user list API 호출

--- js/components/App.js
+++ js/components/App.js
@@ -1,12 +1,16 @@
 import Component from '../core/Component.js';
+import State from '../core/State.js';
 import UserList from './UserList.js';
 
 export default class App extends Component {
+  selectedUser;
   constructor($target, $props, $children) {
     const { $userList } = $children;
 
     super($target, $props);
 
-    new UserList($userList);
+    this.selectedUser = new State('');
+
+    new UserList($userList, { selectedUser: this.selectedUser });
   }
 }
--- js/components/UserList.js
+++ js/components/UserList.js
@@ -1,10 +1,24 @@
 import Component from '../core/Component.js';
+import { API_BASE_URL } from '../constant/index.js';
+import State from '../core/State.js';
 
 export default class UserList extends Component {
+  #users;
+
   constructor($target, props) {
     super($target, props);
+
+    this.#users = new State([]);
+
     this.initEventListener($target);
+    this.loadUsers();
+    this.render();
   }
+
+  loadUsers = async () => {
+    const data = await (await fetch(`${API_BASE_URL}/api/users`)).json();
+    if (data instanceof Array) this.#users.value = data;
+  };
 
   createUser = () => {
     let userName = prompt('추가하고 싶은 이름을 입력해주세요.');
