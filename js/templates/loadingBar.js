1702cf5d97a26f8d979b198b4976ec195f6659fd
jho2301 <jho2301@gmail.com>
2020-09-13 16:15:33 +0900

    detach templates from index.html

---
+++ js/templates/loadingBar.js
@@ -0,0 +1,14 @@
+const createLoadingBar = () =>
+  `
+    <li>
+      <div class="view">
+        <label class="label">
+          <div class="animated-background">
+            <div class="skel-mask-container">
+              <div class="skel-mask"></div>
+            </div>
+          </div>
+        </label>
+      </div>
+    </li>
+`;
---
+++ js/templates/userButton.js
@@ -0,0 +1,7 @@
+{
+  /* <button class="ripple active">eastjun</button>
+<button class="ripple">westjun</button>
+<button class="ripple">southjun</button>
+<button class="ripple">northjun</button>
+<button class="ripple">hojun</button> */
+}
