06011ddbe6e6d19cf71e5bd84b7fff7b02056e87
eyabc <bey4314@naver.com>
2020-09-15 01:12:07 +0900

    feat: 첫 렌더시 User title 변경

--- src/components/User/UserTitle.js
+++ src/components/User/UserTitle.js
@@ -1,8 +1,13 @@
-const UserTitle = ({ name }) => {
-    return `
-      <h1 id="user-title" data-username="eastjun">
-        <span><strong>${name}</strong>'s Todo List</span>
-      </h1>
+import { getter } from '../../store/index.js';
+
+const UserTitle = () => {
+  const userName = getter.userName();
+  const getTitleName = userName ? `${userName}\'s` : '';
+
+  return `
+    <h1 id="user-title" data-username="eastjun">
+      <span><strong>${getTitleName}</strong>Todo List</span>
+    </h1>
   `;
 };
 
