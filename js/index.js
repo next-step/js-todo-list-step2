b9dccd89cb1012b55f97de61dcdf7f07e21d03ec
kingdotol <oh@dotol.net>
2020-09-19 12:19:01 +0900

    template 정리, 모듈 설정

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
-const onUserCreateHandler = () => {
-  const userName = prompt("추가하고 싶은 이름을 입력해주세요.");
+fetch('https://js-todo-list-9ca3a.df.r.appspot.com/api/users/')
+  .then(response => response.json())
+  .then(json => userLoad(json));
+
+function userLoad(users){
+  const userList = document.querySelector("#user-list");
+  const template = users.map(user => userTemplate(user.name));
+  userList.innerHTML = "\n"+template.join("\n")+"\n";
 }
-
-const userCreateButton = document.querySelector('.user-create-button')
-userCreateButton.addEventListener('click', onUserCreateHandler)
-
+function userTemplate(name){
+  return `<button class="ripple">${name}</button>`
+}
