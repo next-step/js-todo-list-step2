0140903562745339cc4e8a805669e01fd8183d9a
eyabc <bey4314@naver.com>
2020-09-20 23:05:13 +0900

    refactor: TodoLabel 도 return { dom, render } 구조로 변경

--- src/template/Todo/TodoItem.js
+++ src/template/Todo/TodoItem.js
@@ -1,6 +1,10 @@
 import TodoLabel from './TodoLabel.js';
 
 const TodoItem = (props) => {
+  const components = {
+    todoLabel: undefined
+  };
+
   const { todo } = props;
   const dom = document.createElement('li');
   dom.dataset.todoIdx = todo._id;
@@ -11,14 +15,17 @@ const TodoItem = (props) => {
     const viewCondition = () => (mode === undefined || mode === 'view');
     const display = (condition) => condition ? 'block' : "none";
 
+    components.todoLabel = TodoLabel({ contents });
+
     dom.innerHTML = `
     <div class="view" style="display: ${display(viewCondition())}">
       <input class="toggle" type="checkbox" data-component="toggleComplete" ${ isCompleted && "checked"}/>
-      ${ TodoLabel({ contents }) }
       <button class="destroy" data-component="deleteItem"></button>
     </div>
     <input class="edit" data-component="editMode" style="display: ${display(!viewCondition())}" value="${ contents }" required/>
   `;
+    dom.querySelector('.view').insertBefore(components.todoLabel.dom, dom.querySelector('.destroy'));
+    components.todoLabel.render();
   };
 
   return { dom, render };
--- src/template/Todo/TodoLabel.js
+++ src/template/Todo/TodoLabel.js
@@ -1,14 +1,20 @@
 const TodoLabel = ({ contents }) => {
-  return `
-  <label class="label" data-component="todo-label">
-    <select class="chip select" data-component="todo-priority"> 
-      <option value="0" selected>순위</option>
-      <option value="1">1순위</option>
-      <option value="2">2순위</option>
-    </select>
-    ${ contents }
-  </label>
-  `;
+  const dom = document.createElement('label');
+  dom.classList.add('label');
+  dom.dataset.component = 'todo-label';
+
+  const render = () => {
+    dom.innerHTML = `
+      <select class="chip select" data-component="todo-priority"> 
+        <option value="0" selected>순위</option>
+        <option value="1">1순위</option>
+        <option value="2">2순위</option>
+      </select>
+      ${ contents }
+    ;`
+  };
+
+  return { dom, render }
 };
 
 // chip primary secondary
