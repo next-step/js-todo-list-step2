325af4d2c8b912fa360e6e0a2bd0d9d2145cafce
김종완 <oen142@naver.com>
2020-10-05 23:43:02 +0900

    await 제대로 쓰기

--- js/service/core.js
+++ js/service/core.js
@@ -3,7 +3,7 @@
 export const request = async (url, option) => {
     try {
         const response = await fetch(url, option);
-        return response.json();
+        return await response.json();
     } catch (e) {
         console.error(e);
     }
