5d62092d6093a961aad3ce75eba36ecfba06f00d
김종완 <oen142@naver.com>
2020-10-05 23:39:21 +0900

    커밋 UserList

--- js/components/user/UserList.js
+++ js/components/user/UserList.js
@@ -15,6 +15,7 @@ const loadingTemplate = () => `
     <li>
         <div class="view">
         <label class="label">
+        
             <div class="animated-background">
                  <div class="skel-mask-container">
                     <div class="skel-mask"></div>
