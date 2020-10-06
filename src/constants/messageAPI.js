4dc27d4d829e77c225c274fc2c4b367306418156
eyabc <bey4314@naver.com>
2020-10-06 15:47:57 +0900

    refactor: export 변경

--- src/constants/messageAPI.js
+++ src/constants/messageAPI.js
@@ -1,4 +1,4 @@
-const ERROR = {
+export const ERROR = {
   NO_USER: '해당 이름의 user가 없습니다.',  // used: createUserTodoItem
   NO_USER2: '해당 user를 찾는데 에러가 발생했습니다.', // readUserTodoItems
   NO_USER3: '해당 유저가 존재하지 않습니다.', // updateUserTodoItemComplete, deleteUserItemService, putUserItemService, deleteUserItemsAllService, updateUserTodoItemPriority
@@ -6,9 +6,8 @@ const ERROR = {
   DELETE_TODO_ITEM: 'Todo Item을 삭제하는데 에러가 발생했습니다.',// deleteUserItemService
 };
 
-const SUCCESS = {
+export const SUCCESS = {
   DELETE_TODO_ITEM_ALL: '해당 유저의 Todo Item 을 모두 삭제하였습니다.'
 };
 
-export { ERROR, SUCCESS };
 
