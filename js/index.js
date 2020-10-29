4560d878779b3c8af830afc9a3c9cfa203e3b066
yujo <yujo@student.42seoul.kr>
2020-10-29 22:25:23 +0900

    turn over

--- js/index.js
+++ js/index.js
@@ -6,9 +6,29 @@ const onUserCreateHandler = () => {
   apiService.addUser(userName);
 };
 
-apiService
-  .fetchUsers()
-  .then((users) => users.map((user) => user.name))
-  .then(console.log);
+/**
+ * 유저 목록을 보여준다
+ * @param {Array} users
+ */
+
+const showUsers = (users) => {
+  for (const user of users) {
+    console.log(user);
+  }
+};
+
+apiService.fetchUsers().then(showUsers);
+
 const userCreateButton = document.querySelector(".user-create-button");
 userCreateButton.addEventListener("click", onUserCreateHandler);
+
+{
+  /* <div id="user-list">
+  <button class="ripple active">eastjun</button>
+  <button class="ripple">westjun</button>
+  <button class="ripple">southjun</button>
+  <button class="ripple">northjun</button>
+  <button class="ripple">hojun</button>
+  <button class="ripple user-create-button">+ 유저 생성</button>
+</div>; */
+}
