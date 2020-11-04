1b7ebbddb200c776122f5a28a4dde3b41c35d433
eyabc <bey4314@naver.com>
2020-11-04 22:34:21 +0900

    refactor: Object.freeze - https://github.com/next-step/js-todo-list-step2/pull/24#discussion_r517341338

--- src/constants/filter.js
+++ src/constants/filter.js
@@ -2,11 +2,10 @@ export const ALL  = 'all';
 export const ACTIVE  = 'active';
 export const COMPLETED  = 'completed';
 
-export const filter = {
+export const filter = Object.freeze({
   [ALL]: '전체보기',
   [ACTIVE]: '해야할 일',
   [COMPLETED]: '완료한 일'
-};
+});
 
-Object.freeze(filter);
 
