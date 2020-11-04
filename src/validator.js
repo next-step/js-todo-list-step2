b038a1469c74446fe124d3b26f0a6f143d62d246
eyabc <bey4314@naver.com>
2020-11-04 17:19:01 +0900

    refactor: if 문 중괄호 - https://github.com/next-step/js-todo-list-step2/pull/24#discussion_r515966737

--- src/validator.js
+++ src/validator.js
@@ -1,6 +1,7 @@
 export const validateUserName = async (name, caller) => {
-  if (name.length > 1)
+  if (name.length > 1) {
     return true;
+  }
 
   alert('User 의 이름은 최소 2글자 이상이어야 합니다.');
   caller(validateUserName);
