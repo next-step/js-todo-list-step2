87a375db36c710a3a15ccf9169ce4574361a2114
eyabc <bey4314@naver.com>
2020-11-04 22:36:24 +0900

    refactor: 주석 삭제 - https://github.com/next-step/js-todo-list-step2/pull/24#discussion_r517332425

--- src/constants/messageAPI.js
+++ src/constants/messageAPI.js
@@ -1,9 +1,9 @@
 export const ERROR = {
-  NO_USER: '해당 이름의 user가 없습니다.',  // used: createUserTodoItem
-  NO_USER2: '해당 user를 찾는데 에러가 발생했습니다.', // readUserTodoItems
-  NO_USER3: '해당 유저가 존재하지 않습니다.', // updateUserTodoItemComplete, deleteUserItemService, putUserItemService, deleteUserItemsAllService, updateUserTodoItemPriority
-  UPDATE_TODO_ITEM: 'Todo Item을 수정하는데 에러가 발생했습니다.', // updateUserTodoItemComplete, putUserItemService, updateUserTodoItemPriority
-  DELETE_TODO_ITEM: 'Todo Item을 삭제하는데 에러가 발생했습니다.',// deleteUserItemService
+  NO_USER: '해당 이름의 user가 없습니다.',
+  NO_USER2: '해당 user를 찾는데 에러가 발생했습니다.',
+  NO_USER3: '해당 유저가 존재하지 않습니다.',
+  UPDATE_TODO_ITEM: 'Todo Item을 수정하는데 에러가 발생했습니다.',
+  DELETE_TODO_ITEM: 'Todo Item을 삭제하는데 에러가 발생했습니다.',
 };
 
 export const SUCCESS = {
