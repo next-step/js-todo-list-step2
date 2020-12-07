601e39777e0a1d53c5df393069c2db8f9a5132f6
sudalqueen <abbey17@cnu.ac.kr>
2020-12-07 22:28:51 +0900

    feat: core/redux-saga 구현 - 인터넷 참고 - waitNextAction 내부는 현 프로젝트 redux에 맞춰 변경

---
+++ src/core/redux-saga/index.js
@@ -0,0 +1,11 @@
+import { take, select, call, put, fork, takeEvery, runSaga } from "./redux-saga.js";
+
+export {
+    take,
+    select,
+    call,
+    put,
+    fork,
+    takeEvery,
+    runSaga,
+}
