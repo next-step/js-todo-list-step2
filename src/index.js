5f91546e8f4e96bda822276990c6448dfc1f844f
sudalqueen <abbey17@cnu.ac.kr>
2020-12-08 16:39:49 +0900

    feat: src/index.js에 새로운 컴포넌트 연결 및 index.html 정리

--- src/index.js
+++ src/index.js
@@ -2,25 +2,33 @@ import { store } from "./modules/index.js";
 import { runSaga } from "./core/redux-saga/index.js";
 import { userSaga } from "./modules/user/index.js";
 import { fetchGetUsers } from "./modules/user/actions.js";
-import { UserList, TodoList, TodoInput } from "./components/index.js";
+import { TodoListTitle, UserList, TodoList, TodoInput, TodoBottom, Loading } from "./components/index.js";
 
 class App {
   constructor() {
+    const todoTitleTarget = document.querySelector('#user-title');
     const userListTarget = document.querySelector('#user-list-section');
     const todoInputTarget = document.querySelector('.new-todo');
     const todoListTarget = document.querySelector('.todo-list');
+    const todoBottomTarget = document.querySelector('.count-container');
+    const loadingTarget = document.querySelector('#loading');
 
+    new TodoListTitle(todoTitleTarget);
     new UserList(userListTarget);
     new TodoInput(todoInputTarget);
     new TodoList(todoListTarget);
+    new TodoBottom(todoBottomTarget);
+    new Loading(loadingTarget);
 
-    runSaga(store, userSaga).then(r => r);
+    runSaga(store, userSaga);
 
     store.subscribe(() => {
-      console.log("state: ", store.getState());
+      new TodoListTitle(todoTitleTarget);
       new UserList(userListTarget);
       new TodoInput(todoInputTarget);
       new TodoList(todoListTarget);
+      new TodoBottom(todoBottomTarget);
+      new Loading(loadingTarget);
     });
 
     store.dispatch(fetchGetUsers.REQUEST());
