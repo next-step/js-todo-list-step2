ac64ad1a0557aa42b064609cd2f7310c32b4e49b
Will <kode15333@gmail.com>
2020-12-10 20:06:48 +0900

    리펙토링 - 불필요한 await 키워드 삭제

--- js/util/index.js
+++ js/util/index.js
@@ -7,50 +7,50 @@ export const getUserList = async () => {
 
 export const addUser = async (userInfo) => {
   const res = await request(HTTP_METHOD.POST, 'api/users', userInfo);
-  return  await res.json();
+  return res.json();
 }
 
 export const getUser = async (userId) => {
   const res = await request(HTTP_METHOD.GET,`api/users/${userId}`);
-  return await res.json();
+  return res.json();
 }
 
 export const deleteUser = async (userId) => {
   const res = await request(HTTP_METHOD.DELETE, `api/users/${userId}`);
-  return await res.json();
+  return res.json();
 }
 
 export const getUserTodo = async (userId) => {
   const res = await request(HTTP_METHOD.GET, `api/users/${userId}/items/`);
-  return await res.json();
+  return res.json();
 }
 
 export const addUserTodo = async (userId, todo) => {
   const res = await request(HTTP_METHOD.POST,`api/users/${userId}/items/`, todo);
-  return await res.json();
+  return res.json();
 }
 export const deleteUserAllTodo = async (userId ) => {
   const res = await request(HTTP_METHOD.DELETE,`api/users/${userId}/items/`);
-  return await res.json();
+  return res.json();
 }
 
 export const deleteUserTodo = async (userId, itemId) => {
   const res = await request(HTTP_METHOD.DELETE,`api/users/${userId}/items/${itemId}`);
-  return await res.json();
+  return res.json();
 }
 export const updateUserTodoContents = async (userId, itemId, todo) => {
   const res = await request(HTTP_METHOD.PUT,`api/users/${userId}/items/${itemId}`, todo);
-  return await res.json();
+  return res.json();
 }
 
 export const updateUserTodoPriority = async (userId, itemId, priority) => {
   const res = await request(HTTP_METHOD.PUT,`api/users/${userId}/items/${itemId}/priority`, priority);
-  return await res.json();
+  return res.json();
 }
 
 export const toggleTodoComplete = async (userId, itemId) => {
   const res = await request(HTTP_METHOD.PUT,`api/users/${userId}/items/${itemId}/toggle`);
-  return await res.json();
+  return res.json();
 }
 
 
