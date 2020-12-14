fb023a61dabae9c9ff0faa17bc86f603a0303a81
hwookim <gusdn9754@gmail.com>
2020-12-14 10:44:15 +0900

    refactor: $store.todo 선언 방식 변경
     - setter를 이용하지 않고 별도 setSelected 함수를 이용해 구독 중인 함수들 실행하도록 변경
     - subscribe, publish 방식으로 구현

--- js/store/module/todo.js
+++ js/store/module/todo.js
@@ -2,57 +2,50 @@ import $api from "../../api/index.js";
 import user from "./user.js";
 
 const todo = (() => {
-  let todos = [];
+  const watch = {};
+
+  const getAll = async () => {
+    const selectedId = await user.getSelectedId();
+    return await $api.todo.getAll(selectedId);
+  };
+
+  const create = async (contents) => {
+    const selectedId = await user.getSelectedId();
+    const newTodo = await $api.todo.create(selectedId, { contents });
+    publish("todos");
+    return newTodo;
+  };
+
+  const deleteTodo = async (todoId) => {
+    const selectedId = await user.getSelectedId();
+    const deletedTodo = await $api.todo.delete(selectedId, todoId);
+    publish("todos");
+    return deletedTodo;
+  };
+
+  const deleteAll = async () => {
+    const selectedId = await user.getSelectedId();
+    await $api.todo.deleteAll(selectedId);
+    publish("todos");
+  };
+
+  const subscribe = (target, method) => {
+    if (!watch[target]) {
+      watch[target] = [method];
+    }
+    watch[target].push(method);
+  };
+
+  const publish = (target) => {
+    watch[target].forEach(async (method) => await method());
+  };
 
   return {
-    get todos() {
-      return todos;
-    },
-    set todos(value) {
-      todos = value;
-      this.watch.todos.forEach((method) => method());
-    },
-
-    subscribe(target, method) {
-      if (!this["watch"]) {
-        this["watch"] = {};
-      }
-      if (!this["watch"][target]) {
-        this.watch[target] = [method];
-      }
-      this.watch[target].push(method);
-    },
-
-    async getAll() {
-      const selectedId = await user.getSelectedId();
-      todos = await $api.todo.getAll(selectedId);
-      return todos;
-    },
-
-    async create(contents) {
-      const selectedId = await user.getSelectedId();
-      const newTodo = await $api.todo.create(selectedId, { contents });
-
-      todos.push(newTodo);
-      this.todos = todos;
-
-      return newTodo;
-    },
-
-    async delete(todoId) {
-      const selectedId = await user.getSelectedId();
-      const deletedTodo = await $api.todo.delete(selectedId, todoId);
-
-      todo.todos = await $api.todo.getAll(selectedId);
-
-      return deletedTodo;
-    },
-
-    async deleteAll() {
-      const selectedId = await user.getSelectedId();
-      await $api.todo.deleteAll(selectedId);
-      todo.todos = [];
-    },
+    getAll,
+    create,
+    delete: deleteTodo,
+    deleteAll,
+    subscribe,
   };
 })();
 
