ac9ac023cc414212de6d3f9af182d701c07b4265
Hyuneui <46099642+hongjehong@users.noreply.github.com>
2021-01-28 16:45:25 +0900

    User 추가하기

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
 const onUserCreateHandler = () => {
   const userName = prompt("추가하고 싶은 이름을 입력해주세요.");
+
+  const $newUser = document.createElement('button');
+  $newUser.className = 'ripple'
+  $newUser.innerText = userName;
+
+  const userCreateButton = document.querySelector('button.user-create-button');
+  userCreateButton.insertAdjacentElement('beforebegin', $newUser);
 }
 
-const userCreateButton = document.querySelector('.user-create-button')
-userCreateButton.addEventListener('click', onUserCreateHandler)
-
+const userCreateButton = document.querySelector('.user-create-button');
+userCreateButton.addEventListener('click', onUserCreateHandler);
