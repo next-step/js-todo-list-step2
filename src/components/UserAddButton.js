96145f8da0fc99d73143a9109d9ebe448a941be8
yujo <yujo@student.42seoul.kr>
2021-01-28 23:00:14 +0900

    feat: event delegation for user create button

---
+++ src/components/UserAddButton.js
@@ -0,0 +1,7 @@
+class UserAddButton {
+  static render() {
+    return `<button class="ripple user-create-button">+ 유저 생성</button>`;
+  }
+}
+
+export default UserAddButton;
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,4 +1,5 @@
 import api from '../api/api.js';
+import UserAddButton from './UserAddButton.js';
 import UserItem from './UserItem.js';
 
 const MINIMUN_USER_LENGTH = 2;
@@ -6,27 +7,30 @@ const MINIMUN_USER_LENGTH = 2;
 class UserList {
   constructor() {
     this.$userList = document.querySelector('#user-list');
-    this.$userCreateButton = document.querySelector('.user-create-button');
   }
 
   init() {
     this.renderUsers();
-    this.$userCreateButton.addEventListener('click', this.addUser);
+    this.$userList.addEventListener('click', this.addUser);
   }
 
   async renderUsers() {
     const users = await api.getUsers();
-    const usersHTML = users.map((user) => UserItem.render(user.name));
-    this.$userList.insertAdjacentHTML('afterbegin', usersHTML.join('\n'));
+    const userButtons = users.map((user) => UserItem.render(user.name));
+    userButtons.push(UserAddButton.render());
+    this.$userList.insertAdjacentHTML('afterbegin', userButtons.join('\n'));
   }
 
-  async addUser() {
-    const userName = prompt('이름을 입력해주세요');
-    userName.trim();
+  async addUser({ target }) {
+    if (target.classList.contains('user-create-button')) {
+      const userName = prompt('이름을 입력해주세요');
+      userName.trim();
 
-    if (userName.length > MINIMUN_USER_LENGTH) {
-      const newUser = await api.addUser(userName);
-      this.renderUsers();
+      if (userName.length > MINIMUN_USER_LENGTH) {
+        // const newUser = await api.addUser(userName);
+        await api.addUser(userName);
+        this.renderUsers();
+      }
     }
   }
 }
