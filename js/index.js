7e7a119f03fccc95c3638178e08cbcd196f0107f
skid901 <tlrudejr1993@naver.com>
2021-01-28 22:32:57 +0900

    feat: migrate week 1 mission output

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
+import TodoApp from "./components/TodoApp.js";
+
+const appEl = document.querySelector("#app");
+new TodoApp(appEl).render();
+
 const onUserCreateHandler = () => {
   const userName = prompt("추가하고 싶은 이름을 입력해주세요.");
-}
+};
 
-const userCreateButton = document.querySelector('.user-create-button')
-userCreateButton.addEventListener('click', onUserCreateHandler)
-
+const userCreateButton = document.querySelector(".user-create-button");
+userCreateButton.addEventListener("click", onUserCreateHandler);
