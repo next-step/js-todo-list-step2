9410c5a1288e11b92257afe7db437360532864a6
Hyuneui <46099642+hongjehong@users.noreply.github.com>
2021-01-28 21:06:28 +0900

    User의 투두리스트 불러오기

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
+let currentUser = '';
+let todoList = [];
+let todoComplete = [];
+let todoFilter = 'all';
+
 const onUserCreateHandler = () => {
   const userName = prompt("추가하고 싶은 이름을 입력해주세요.");
 
@@ -10,4 +15,86 @@ const onUserCreateHandler = () => {
 }
 
 const userCreateButton = document.querySelector('.user-create-button');
+const userList = document.querySelector('div#user-list');
+
 userCreateButton.addEventListener('click', onUserCreateHandler);
+userList.addEventListener('click', event => {
+  userList.querySelectorAll('button.ripple').forEach($user => onUserSelectHandler($user, event));
+})
+
+window.onload = () => {
+  const currentUserButton = document.querySelector('button.ripple.active');
+  currentUser = (currentUserButton) ? currentUserButton.innerText : document.querySelector('button.ripple');
+  todoList = localStorage.getItem(currentUser + '_todoList') ?? [];
+  todoComplete = localStorage.getItem(currentUser + '_todoComplete') ?? [];
+  render();
+}
+
+const onUserSelectHandler = ($user, event) => {
+  if(!$user.contains(event.target)){
+    return;
+  }
+  const currentUserButton = document.querySelector('button.ripple.active');
+  if(currentUserButton) currentUserButton.classList.remove('active');
+  $user.classList.add('active');
+
+  currentUser = $user.innerText;
+  todoList = localStorage.getItem(currentUser + '_todoList') ?? [];
+  todoComplete = localStorage.getItem(currentUser + '_todoComplete') ?? [];
+  render();
+}
+
+const render = () => {
+  const $todoList = document.querySelector('ul.todo-list');
+
+  const todos = filterList();
+  const renderList = todos.map(($item, index) => {
+    if(!todoComplete[index]){
+      return activeTemplate($item);
+    }
+    return completedTemplate($item);
+  })
+  $todoList.innerHTML = renderList.join(' ');
+
+}
+
+const filterList = () => {
+  if(todoFilter === 'all') {
+    return todoList;
+  }
+  if(todoFilter === 'active'){
+    return todoList.filter(($item, index) => !todoComplete[index]);
+  }
+  if(todoFilter === 'completed'){
+    return todoList.filter(($item, index) => todoComplete[index]);
+  }
+}
+
+const activeTemplate = todoItem => `
+        <li>
+          <div class="view">
+            <input class="toggle" type="checkbox" />
+            <label class="label">
+              <select class="chip select">
+                <option value="0" selected>순위</option>
+                <option value="1">1순위</option>
+                <option value="2">2순위</option>
+              </select>
+              ${todoItem}
+            </label>
+            <button class="destroy"></button>
+          </div>
+          <input class="edit" value=${todoItem} />
+        </li>
+`;
+
+const completedTemplate = todoItem => `
+        <li class="completed">
+          <div class="view">
+            <input class="toggle" type="checkbox" checked />
+            <label class="label">${todoItem} </label>
+            <button class="destroy"></button>
+          </div>
+          <input class="edit" value=${todoItem} />
+        </li>
+`;
