028458a5423e7cc34463c0389500207bdbb0a2bb
hwookim <gusdn9754@gmail.com>
2021-02-01 03:29:30 +0900

    refactor: $div => div

--- src/utils/createElement.js
+++ src/utils/createElement.js
@@ -1,8 +1,8 @@
 const createElement = (htmlString) => {
-  const $div = document.createElement("div");
-  $div.innerHTML = htmlString.trim();
+  const div = document.createElement("div");
+  div.innerHTML = htmlString.trim();
 
-  return $div.firstChild;
+  return div.firstChild;
 };
 
 export { createElement };
