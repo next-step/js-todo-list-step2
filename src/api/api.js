74cf1851eb57cb60427153137e967f1b19723129
SoHyun <pje1740@gmail.com>
2021-02-01 17:04:22 +0900

    [Feat] :zap:️ add getUser api call

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,7 @@
 const BASE_URL = 'https://js-todo-list-9ca3a.df.r.appspot.com';
 const ADD_USER = '/api/users';
 const GET_USERS = '/api/users';
+const GET_USER = '/api/users/';
 
 const addUser = async (name) => {
   try {
@@ -36,7 +37,24 @@ const getUsers = async () => {
   }
 };
 
+const getUser = async (userId) => {
+  try {
+    const response = await fetch(`${BASE_URL}${GET_USER}${userId}`, {
+      method: 'GET',
+    });
+
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  } catch (err) {
+    alert(err);
+    return err;
+  }
+};
+
 export default {
   addUser,
   getUsers,
+  getUser,
 };
