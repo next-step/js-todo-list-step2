955f2d4bea857d3fccb184c0924bdb424f4b872d
domuk-k <dannyworks102@gmail.com>
2021-02-02 15:23:39 +0900

    deleted: directory replaced

--- js/index.js
+++
@@ -1,5 +0,0 @@
-import ReillyDOM from './lib/reilly/ReillyDOM.js';
-import { createElement } from './lib/reilly/Reilly.js';
-import App from './App.js';
-
-ReillyDOM.render(createElement(App), document.getElementById('root'));
