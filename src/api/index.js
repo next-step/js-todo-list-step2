0f88089654b6968eb37f1cccca37c3fbead08130
hwookim <gusdn9754@gmail.com>
2021-02-05 16:35:52 +0900

    refactor: api uri 표현 방식 변경

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,10 @@
 import { HTTP_METHOD } from "./utils/constants.js";
 
-const BASE_URL = "https://js-todo-list-9ca3a.df.r.appspot.com/api/users";
+const BASE_URL = "https://js-todo-list-9ca3a.df.r.appspot.com/api/";
 
 const api = (() => {
-  const request = (uri, config) => fetch(uri, config).catch(console.error);
+  const request = (uri, config) =>
+    fetch(BASE_URL + uri, config).catch(console.error);
 
   const requestWithJsonData = (uri, config) =>
     fetch(uri, config)
@@ -12,53 +13,50 @@ const api = (() => {
 
   const user = {
     getAll() {
-      return requestWithJsonData(BASE_URL);
+      return requestWithJsonData(`users`);
     },
     create(data) {
-      return requestWithJsonData(BASE_URL, HTTP_METHOD.POST(data));
+      return requestWithJsonData(`users`, HTTP_METHOD.POST(data));
     },
     delete(id) {
-      return request(BASE_URL + `/${id}`, HTTP_METHOD.DELETE());
+      return request(`users/${id}`, HTTP_METHOD.DELETE());
     },
   };
 
   const todo = {
     getAll(userId) {
-      return requestWithJsonData(BASE_URL + `/${userId}/items`);
+      return requestWithJsonData(`users/${userId}/items`);
     },
     create(userId, data) {
       return requestWithJsonData(
-        BASE_URL + `/${userId}/items`,
+        `users/${userId}/items`,
         HTTP_METHOD.POST(data)
       );
     },
     delete(userId, todoId) {
       return requestWithJsonData(
-        BASE_URL + `/${userId}/items/${todoId}`,
+        `users/${userId}/items/${todoId}`,
         HTTP_METHOD.DELETE()
       );
     },
     deleteAll(userId) {
-      return requestWithJsonData(
-        BASE_URL + `/${userId}/items`,
-        HTTP_METHOD.DELETE()
-      );
+      return requestWithJsonData(`users/${userId}/items`, HTTP_METHOD.DELETE());
     },
     toggle(userId, todoId) {
       return requestWithJsonData(
-        BASE_URL + `/${userId}/items/${todoId}/toggle`,
+        `users/${userId}/items/${todoId}/toggle`,
         HTTP_METHOD.PUT()
       );
     },
     edit(userId, todoId, data) {
       return requestWithJsonData(
-        BASE_URL + `/${userId}/items/${todoId}`,
+        `users/${userId}/items/${todoId}`,
         HTTP_METHOD.PUT(data)
       );
     },
     priority(userId, todoId, data) {
       return requestWithJsonData(
-        BASE_URL + `/${userId}/items/${todoId}/priority`,
+        `users/${userId}/items/${todoId}/priority`,
         HTTP_METHOD.PUT(data)
       );
     },
