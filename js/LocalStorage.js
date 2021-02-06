fdce63ab1d55a871f058d525499d314a1c9aa2c8
jjunyjjuny <jjuny546@gmail.com>
2021-02-06 23:44:53 +0900

    Add fetch remain POST, PUT을 사용한 각종 요청들 구현 완료. 중복 내용은 추후 수정 예정

--- js/LocalStorage.js
+++ js/LocalStorage.js
@@ -1,23 +1,15 @@
-const defaultUserList = [
-  {
-    _id: "default",
-    name: "default_user",
-  },
-];
-
 export const getUserListByLocalStorage = () => {
   const userList = JSON.parse(localStorage.getItem("userlist"));
-  console.log(userList);
-  return userList.length !== 0 ? userList : defaultUserList;
+  if (!userList) return false;
+  return userList;
 };
 
 export const setUserListToLocalStorage = () => {
-  console.log("set!");
   localStorage.removeItem("userlist");
   const userList = [...document.querySelectorAll("#user-list>.userBtn")];
   const dataset = userList.reduce((acc, curr) => {
     const form = {
-      id: curr.getAttribute("id"),
+      _id: curr.getAttribute("id"),
       name: curr.innerText,
     };
     acc.push(form);
--- js/UserList.js
+++ js/UserList.js
@@ -3,42 +3,80 @@ import {
   setUserListToLocalStorage,
 } from "./LocalStorage.js";
 
-import { postUser } from "./Fetch.js";
+import {
+  postUser,
+  deleteUser,
+  getTodoItems,
+  postTodoItem,
+  deleteTodoItems,
+  deleteTodoItem,
+  reviseTodoItem,
+  revisePriorityOfTodoItem,
+  toggleCompleteOfTodoItem,
+} from "./Fetch.js";
 
 export const initUserList = () => {
   const userList = getUserListByLocalStorage();
-  const target = document.querySelector(".user-create-button");
-  userList.map((user) => {
-    addUserBtnToUserList(user, target);
-  });
-  const userCreateBtn = document.querySelector(".user-create-button");
-  const userListDIV = document.getElementById("user-list");
+  const $target = document.querySelector(".user-create-button");
 
-  userCreateBtn.addEventListener("click", onUserCreateHandler);
+  if (userList) {
+    userList.map((user) => {
+      addUserBtnToUserList(user, $target);
+    });
+  }
+  const $userCreateBtn = document.querySelector(".user-create-button");
+  const $userList = document.getElementById("user-list");
+  $userList.addEventListener("click", renderTodoListOfUser);
+
+  $userCreateBtn.addEventListener("click", onUserCreateHandler);
 };
 
 const onUserCreateHandler = async ({ target }) => {
   const userName = prompt("추가하고 싶은 이름을 입력해주세요.");
+  if (!userName) return;
   const user = await postUser(userName);
   addUserBtnToUserList(user, target);
   setUserListToLocalStorage();
 };
 function addUserBtnToUserList(user, target) {
-  const userBtn = createUserButton(user);
+  const $userBtn = createUserButton(user);
 
-  userBtn.setAttribute("id", user._id);
-  userBtn.innerText = user.name;
+  $userBtn.setAttribute("id", user._id);
+  $userBtn.innerText = user.name;
 
-  const userListDIV = document.getElementById("user-list");
+  const $userList = document.getElementById("user-list");
 
-  userListDIV.insertBefore(userBtn, target);
+  $userList.insertBefore($userBtn, target);
 }
 const createUserButton = (user) => {
-  const button = document.createElement("button");
-  button.classList.add("ripple", "userBtn");
+  const $button = document.createElement("button");
+  $button.classList.add("ripple", "userBtn");
+  $button.setAttribute("id", user._id);
+  $button.innerText = user.name;
+  return $button;
+};
 
-  button.setAttribute("id", user.id);
-  button.innerText = user.name;
+const renderTodoListOfUser = async ({ target }) => {
+  const classes = target.className.split(" ");
+  if (!classes.includes("userBtn")) return false;
+  const todoList = await getTodoItems(target.id);
+  assignTodoList(todoList);
+};
 
-  return button;
+const assignTodoList = (todolist) => {};
+
+const createTodoItem = (todolist) => {
+  const todoItem = `
+    <li>
+      <div class="view">
+        <input class="toggle" type="checkbox" />
+        <label class="label">
+          <span class="chip secondary">2순위</span>
+           해야할 아이템
+        </label>
+        <button class="destroy"></button>
+      </div>
+      <input class="edit" value="완료된 타이틀" />
+    </li>
+  `;
 };
