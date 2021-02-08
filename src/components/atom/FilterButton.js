26776dd7c7846cdc2c4bd31bc50ee2212e235df4
domuk-k <dannyworks102@gmail.com>
2021-02-09 01:12:34 +0900

    refactor: deleted unused deps

--- src/components/atom/FilterButton.js
+++ src/components/atom/FilterButton.js
@@ -1,12 +1,11 @@
 /*@jsx Reilly.createElement*/
 import Reilly from 'reilly';
-import cn from 'classnames';
 
 function FilterButton({ name, mode, onModeChange, content }) {
   return (
     <a
       href={'#' + name}
-      className={cn(name, { selected: mode === name })}
+      className={`${name} ${mode === name ? 'selected' : ''}`}
       onclick={onModeChange}
     >
       {content}
