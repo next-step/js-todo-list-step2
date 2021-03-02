eec66568ce191781655ab280910010abb4756914
YunJin Choi <zzdd1558@gmail.com>
2021-03-03 01:07:33 +0900

    feature document.querySelector, document.querySelectorAll Utils

---
+++ js/utils/Selector.js
@@ -0,0 +1,11 @@
+
+export default class $ {
+
+  static single(selector) {
+    return document.querySelector(selector);
+  }
+
+  static multi(selector) {
+    return [...document.querySelectorAll(selector)];
+  }
+}
