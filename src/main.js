e42da2b7c8a0a9a7a1f41c4d1b7bf58e128bf1c7
younoah <kr_uno_10@naver.com>
2021-03-03 21:57:38 +0900

    feat : localStorage 유틸 추가

--- src/main.js
+++ src/main.js
@@ -1,1 +1,3 @@
 'use strict';
+
+console.log('테스트');
---
+++ src/utils/localStorage.js
@@ -0,0 +1,11 @@
+'use strict';
+
+export const CURRENT_USER = 'currentUser';
+
+export const setCurrentUser = currentUser => {
+  localStorage.setItem(CURRENT_USER, currentUser);
+};
+
+export const getCurrentUser = () => {
+  return localStorage.getItem(CURRENT_USER) ?? '';
+};
