1bb3ac648dcea470788cb50cf48d3df548d13d6e
younoah <kr_uno_10@naver.com>
2021-03-04 21:45:43 +0900

    feat : LocalStorage 클래스 구현, 저장&불러오기 메서드 구현

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -1,11 +1,24 @@
 'use strict';
 
-export const CURRENT_USER = 'currentUser';
+// export const CURRENT_USER = 'currentUser';
 
-export const setCurrentUser = currentUser => {
-  localStorage.setItem(CURRENT_USER, currentUser);
-};
+// export const setCurrentUser = currentUser => {
+//   localStorage.setItem(CURRENT_USER, currentUser);
+// };
 
-export const getCurrentUser = () => {
-  return localStorage.getItem(CURRENT_USER) ?? '';
-};
+// export const getCurrentUser = () => {
+//   return localStorage.getItem(CURRENT_USER) ?? '';
+// };
+
+class LocalStorage {
+  saveItems() {
+    localStorage.setItem('todoItems', JSON.stringify(this.todoItems));
+  }
+
+  loadItems() {
+    const loadedTodoItems = localStorage.getItem('todoItems');
+    const parsedTodoItems = JSON.parse(loadedTodoItems);
+    if (!parsedTodoItems) return [];
+    return parsedTodoItems;
+  }
+}
