79d19a0ce5e79f1857c9d8e99ffd0f09aebb9a5e
younoah <kr_uno_10@naver.com>
2021-03-06 19:42:15 +0900

    feat : userTitleView 구현

---
+++ src/components/userList/view/userTitleView.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import { $ } from '../../../utils/dom.js';
+
+class UserTitleView {
+  constructor() {
+    this.$userTitle = $('.user-title');
+  }
+
+  render(userName) {
+    this.$userTitle.innerHTML = userTitleTemplate(userName);
+  }
+
+  userTitleTemplate(userName) {
+    return `
+		<span>
+			<strong>${userName}</strong>'s Todo List
+		</span>
+	`;
+  }
+}
+
+export const userTitleView = new UserTitleView();
