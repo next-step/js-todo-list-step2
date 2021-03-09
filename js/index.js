a1629e8c0b8884c85469ad139a02d2139258fda3
Hyuneui <46099642+hongjehong@users.noreply.github.com>
2021-03-09 20:19:55 +0900

    [6기 hongjehong] TodoList with Ajax (#53)
    * Create README.md
    
    * feat(user-create-button): add user create button
    
    * Update README.md
    
    * Update README.md
    
    * User 추가하기
    
    * User의 투두리스트 불러오기
    
    * fix : User 목록 불러오기, User마다 투두리스트 불러오기
    
    * feat : todolist 불러오기
    
    * feat : User 삭제 메소드 구현
    
    * feat: todolist 렌더링
    
    * feat : todoItem 추가
    
    * feat : toggle completedness
    
    * feat : todoItem 삭제
    
    * feat : update todo contents
    
    * refactor : async await
    
    * refactor : API, inline functions, etc
    
    * feat : 로딩 화면 구현
    
    * feat : 유저 삭제 버튼 추가
    
    * feat : 우선순위 수정 및 색깔 표시
    
    Co-authored-by: eastjun <56821976+EastjunDev@users.noreply.github.com>
    Co-authored-by: EastjunDev <eastjun.dev@gmail.com>

---
+++ js/index.js
@@ -0,0 +1,321 @@
+import { API } from './API.js';
+import { userTemplate, itemTemplate } from './template.js';
+
+let currentUser = '';
+let currentUserID = '';
+let users = [];
+let todoList = [];
+let todoFilter = 'all';
+
+const $userTitle = document.querySelector('h1#user-title');
+const userCreateButton = document.querySelector('.user-create-button');
+const userDeleteButton = document.querySelector('.user-delete-button');
+const $todoList = document.querySelector('ul.todo-list');
+const $userList = document.querySelector('div#user-list');
+let currentUserButton = $userList.querySelectorAll('button.ripple')[0];
+
+const $newTodo = document.querySelector('input.new-todo');
+
+userCreateButton.addEventListener('click', async () => onUserCreateHandler());
+userDeleteButton.addEventListener('click', async () => onUserDeleteHandler());
+$userList.addEventListener('click', event => selectUser(event));
+$todoList.addEventListener('click', event => toggleList(event));
+$todoList.addEventListener('click', event => deleteList(event));
+$todoList.addEventListener('dblclick', event => convertToEditing(event));
+$newTodo.addEventListener('keyup', event => getEnterForAddTodo(event));
+$todoList.addEventListener('keyup', event=> modifyList(event));
+$todoList.addEventListener('change', event => priorityChangeOfList(event));
+
+window.onload = () => {
+  loadUserList();
+}
+
+const loadUserList = () => {
+  renderLoading();
+  API.fetchUserList()
+    .then(response => response.json())
+    .then(data => {
+      users = data;
+      init();
+    })
+    .catch(err => console.error(err));
+}
+
+const init = () => {
+  renderUsers();
+  currentUserButton = $userList.querySelectorAll('button.ripple')[0];
+  onUserSelectHandler(currentUserButton, 0);
+  render();
+}
+
+const renderUsers = () => {
+  users.forEach(user => addUser(user));
+
+  const currentUserButton = document.querySelector('button.ripple.active');
+  if(currentUserButton){
+    currentUser = currentUserButton.innerText;
+  }
+  else{
+    currentUser = document.querySelector('button.ripple');
+    currentUser.classList.add('active');
+  }
+}
+
+const onUserCreateHandler = async () => {
+  const userName = prompt("추가하고 싶은 이름을 입력해주세요.");
+  const MINIMUM_USER_NAME_LENGTH = 2;
+  if(userName.length < MINIMUM_USER_NAME_LENGTH){
+      return alert(`User의 이름은 최소 ${MINIMUM_USER_NAME_LENGTH}글자 이상이어야 합니다.`);
+  }
+  currentUserButton = document.querySelector('button.ripple.active');
+
+  const newUser = userTemplate;
+  newUser.name = userName;
+  newUser.todoList = [];
+  newUser._id = Math.random().toString(36).substr(2,10);
+  users.push(newUser);
+  addUser(newUser);
+  
+  await API.addUser(newUser);
+
+  if(currentUserButton){
+    currentUser = currentUserButton.innerText;
+  }
+  else{
+    currentUser = document.querySelector('button.ripple');
+    currentUser.classList.add('active');
+  }
+}
+
+const addUser = user => {
+  const newUserButton = document.createElement('button');
+  newUserButton.classList.add('ripple');
+  newUserButton.innerText = user.name;
+  userCreateButton.insertAdjacentElement('beforebegin', newUserButton);
+}
+
+const onUserDeleteHandler = async () => {
+  renderLoading();
+
+  await API.deleteUser(currentUserID);
+  
+  $userList.removeChild(currentUser);
+  render();
+}
+
+const selectUser = event => {
+  $userList.querySelectorAll('button.ripple').forEach(($user, index) => {
+    if($user.contains(event.target) 
+      && !$user.classList.contains('user-create-button') && !$user.classList.contains('user-delete-button')){
+      onUserSelectHandler($user, index);
+    }
+  });
+}
+
+const toggleList = event => {
+  $todoList.querySelectorAll('input.toggle').forEach(($item, index) => {
+    if($item.contains(event.target)){
+      toggleTodo($item, index);
+    }
+  })
+};
+
+const deleteList = event => {
+  $todoList.querySelectorAll('button.destroy').forEach(($item, index) => {
+    if($item.contains(event.target)){
+      deleteTodo($item, index);
+    }
+  });
+}
+
+const priorityChangeOfList = event => {
+  $todoList.querySelectorAll('select').forEach(($item, index) => {
+    if($item.contains(event.target)){
+      setPriority($item, index);
+    }
+  });
+}
+
+const onUserSelectHandler = async ($user, index) => {
+  $userTitle.innerHTML = `<span><strong>${users[index].name}</strong>'s Todo List</span>`;
+  renderLoading();
+  const currentUserButton = document.querySelector('button.ripple.active');
+  if(currentUserButton) currentUserButton.classList.remove('active');
+  $user.classList.add('active');
+
+  currentUser = $user;
+  currentUserID = users[index]._id;
+
+  const res = await API.fetchTodoList(currentUserID);
+  todoList = await res.json();
+  render();
+}
+
+const toggleTodo = async ($item, index) => {
+  renderLoading();
+  const todoItem = todoList[index];
+  todoItem.isCompleted = !todoItem.isCompleted;
+  todoList[index] = todoItem;
+
+  await API.toggleTodo(currentUserID, todoItem._id);
+  render();
+}
+
+const render = () => {
+  const $todoList = document.querySelector('ul.todo-list');
+
+  const todos = filterList();
+  const renderList = todos.map(($item, index) => {
+    return todoTemplate($item);
+  })
+  $todoList.innerHTML = renderList.join(' ');
+}
+
+const getEnterForAddTodo = event => {
+  if(event.key === 'Enter'){
+    addTodo($newTodo.value);
+  }
+}
+
+const modifyList = event => {
+  if(event.key === 'Enter'){
+    $todoList.querySelectorAll('input.edit').forEach(($item, index) => {
+      const itemElement = $item.closest('li');
+      if(itemElement.classList.contains('editing')){
+        modifyTodo($item, index);
+      }
+    })
+  }
+}
+
+const addTodo = async todoItem => {
+  renderLoading();
+  const newItem = itemTemplate;
+  newItem._id = Math.random().toString(36).substr(2,10);
+  newItem.contents = todoItem;
+  
+  todoList.push(newItem);
+
+  render();
+
+  await API.addTodo(currentUserID, newItem);
+}
+
+const deleteTodo = async ($item, index) => {
+  renderLoading();
+  const todoItem = todoList[index];
+  todoList.splice(index, 1);
+  
+  await API.deleteTodo(currentUserID, todoItem._id);
+  render();
+}
+
+const modifyTodo = async ($item, index) => {
+  renderLoading();
+  const itemElement = $item.closest('li');
+  itemElement.classList.remove('editing');
+  
+  const todoItem = todoList[index];
+  todoItem.contents = $item.value;
+  render();
+
+  await API.updateTodo(currentUserID, todoItem);
+}
+
+const convertToEditing = event => {
+  $todoList.querySelectorAll('label.label').forEach(($item, index) => {
+    if($item.contains(event.target)){
+      const itemElement = $item.closest('li');
+      itemElement.classList.add('editing');
+    }
+  });
+}
+
+const setPriority = async ($item, index) => {
+  const $options = $item.querySelectorAll('option');
+  let $prevSelected, $newSelected;
+  $options.forEach($option => {
+    if($option.selected){
+      $prevSelected = $option;
+    }
+    if($option.value === $item.value){
+      $newSelected = $option;
+    }
+  });
+  
+  const todoItem = todoList[index];
+
+  if($item.value === "0"){
+    todoItem.priority = "NONE";
+    if($item.classList.contains('primary')){
+      $item.classList.remove('primary');
+    }
+    if($item.classList.contains('secondary')){
+      $item.classList.remove('secondary');
+    }
+  }
+  else if($item.value === "1"){
+    todoItem.priority = "FIRST";
+    if($item.classList.contains('secondary')){
+      $item.classList.remove('secondary');
+    }
+    $item.classList.add('primary');
+  }
+  else if($item.value === "2"){
+    todoItem.priority = "SECOND";
+    if($item.classList.contains('primary')){
+      $item.classList.remove('primary');
+    }
+    $item.classList.add('secondary');
+  }
+  await API.updatePriority(currentUserID, todoItem);
+
+  $prevSelected.selected = false;
+  $newSelected.selected = true;
+}
+
+const filterList = () => {
+  if(todoFilter === 'all') {
+    return todoList;
+  }
+  if(todoFilter === 'active'){
+    return todoList.filter(($item) => !$item.isCompleted);
+  }
+  if(todoFilter === 'completed'){
+    return todoList.filter(($item) => $item.isCompleted);
+  }
+}
+
+const renderLoading = () => {
+  $todoList.innerHTML = loadingTemplate;
+}
+
+const todoTemplate = todoItem => `
+        <li class=${(todoItem.isCompleted) ? "completed" : ""}>
+          <div class="view">
+            <input class="toggle" type="checkbox" ${(todoItem.isCompleted) ? 'checked' : ''}/>
+            <label class="label">
+              <select class="chip select ${(todoItem.priority === "FIRST") ? 'primary' : (todoItem.priority === "SECOND") ? 'secondary' : ''}">
+                <option value="0" ${(todoItem.priority === "NONE") ? 'selected' : ''}>순위</option>
+                <option value="1" ${(todoItem.priority === "FIRST") ? 'selected' : ''}>1순위</option>
+                <option value="2" ${(todoItem.priority === "SECOND") ? 'selected' : ''}>2순위</option>
+              </select>
+              ${todoItem.contents}
+            </label>
+            <button class="destroy"></button>
+          </div>
+          <input class="edit" value=${todoItem.contents} />
+        </li>
+`;
+
+const loadingTemplate = `<li>
+<div class="view">
+  <label class="label">
+    <div class="animated-background">
+      <div class="skel-mask-container">
+        <div class="skel-mask"></div>
+      </div>
+    </div>
+  </label>
+</div>
+</li>`;
---
+++ js/template.js
@@ -0,0 +1,12 @@
+export const userTemplate = {
+    _id: '',
+    name: '',
+    todoList: [],
+}
+
+export const itemTemplate = {
+    "_id": "",
+    "contents": "",
+    "priority": "NONE",
+    "isCompleted": false,
+}
