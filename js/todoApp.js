d0fdd04b0000cab3651ef26ab1cdeb3e495ffea7
sujieim <sujieim@gmail.com>
2021-03-11 19:09:56 +0900

    refactor : remove userList - store dependency

--- js/todoApp.js
+++ js/todoApp.js
@@ -1,14 +1,41 @@
-import { todoItemStore } from './store.js';
+import { userStore, todoItemStore } from './store.js';
 
 const $todoApp = document.querySelector('section.todoapp');
 const $listContainer = $todoApp.querySelector('section.main');
 const $listUl = $listContainer.querySelector('.todo-list');
 
 const todoApp = (userList, todoInput, todoList, todoStatus) => {
-  let _todoItems = new Map();
   let _currentFilter = 'all';
 
-  const _store = todoItemStore();
+  const _todoItemStore = todoItemStore();
+  const _userStore = userStore();
+
+  const getUsers = async () => {
+    return await _userStore.getUsers();
+  };
+
+  const addUser = async (name) => {
+    await _userStore.createUser(name);
+    _setUserListState();
+  };
+
+  const removeUser = async () => {
+    await _userStore.deleteUser();
+    _setUserListState();
+    todoListHandler.refresh([]);
+    userListHandler.setTitleName();
+  };
+
+  const selectUser = async (userId) => {
+    const user = await _userStore.setUser(userId);
+    _setTodoListState();
+    userListHandler.setTitleName(user.name);
+  };
+
+  const _setUserListState = async () => {
+    const userList = await getUsers();
+    userListHandler.listUsers(userList);
+  };
 
   const _filterStatusPredicate = {
     //TODO sync with todoStatus.js/filters
@@ -18,33 +45,33 @@ const todoApp = (userList, todoInput, todoList, todoStatus) => {
   };
 
   const addTodoItem = async (contents) => {
-    await _store.createTodo(contents);
-    _setState();
+    await _todoItemStore.createTodo(contents);
+    _setTodoListState();
   };
 
   const removeTodoItem = async ({ _id }) => {
-    await _store.deleteTodo(_id);
-    _setState();
+    await _todoItemStore.deleteTodo(_id);
+    _setTodoListState();
   };
 
   const updateTodoContents = async ({ _id, contents }) => {
-    await _store.updateTodoContents(_id, contents);
-    _setState();
+    await _todoItemStore.updateTodoContents(_id, contents);
+    _setTodoListState();
   };
 
   const updateToggle = async ({ _id }) => {
-    await _store.updateTodoToggle(_id);
-    _setState();
+    await _todoItemStore.updateTodoToggle(_id);
+    _setTodoListState();
   };
 
   const setFilter = (filterType) => {
     _currentFilter = filterType;
-    _setState();
+    _setTodoListState();
   };
 
-  const _setState = async () => {
+  const _setTodoListState = async () => {
     todoListHandler.loading();
-    const todoList = await _store.getTodoList();
+    const todoList = await _todoItemStore.getTodoList();
     _applyFilter(todoList);
   };
 
@@ -56,7 +83,7 @@ const todoApp = (userList, todoInput, todoList, todoStatus) => {
     todoStatusHandler.updateCount(filteredItems.length);
   };
 
-  const userListHandler = userList(_setState);
+  const userListHandler = userList(addUser, removeUser, selectUser);
   const todoInputHandler = todoInput(addTodoItem);
   const todoListHandler = todoList(
     $listUl,
@@ -67,9 +94,7 @@ const todoApp = (userList, todoInput, todoList, todoStatus) => {
   const todoStatusHandler = todoStatus($listUl, setFilter);
 
   const init = () => {
-    userListHandler.init();
-    todoListHandler.refresh(Array.from(_todoItems.values));
-    todoInputHandler.focus();
+    _setUserListState();
   };
 
   return {
--- js/userList.js
+++ js/userList.js
@@ -1,43 +1,35 @@
-import { userStore } from './store.js';
-
-const userList = (setState) => {
+const userList = (createUser, deleteUser, selectUser) => {
   const $userTitle = document.getElementById('user-title');
   const $userList = document.getElementById('user-list');
 
-  const _store = userStore();
-
-  const _onUserCreateHandler = async () => {
+  const _onUserCreateHandler = () => {
     const name = prompt('추가하고 싶은 이름을 입력해주세요.');
     if (name.length < 2) {
       alert('2글자 이상 입력해주세요');
       return;
     }
-    await _store.createUser(name);
-    _listUsers();
+    createUser(name);
   };
 
-  const _onUserDeleteHandler = async () => {
+  const _onUserDeleteHandler = () => {
+    if (!$userTitle.dataset.username) {
+      alert('선택된 유저가 없습니다.');
+      return;
+    }
     if (!confirm('정말로 삭제하시겠습니까?')) {
       return;
     }
 
-    await _store.deleteUser();
-    _listUsers();
+    deleteUser();
   };
 
-  const _onClickUserHandler = async ({ target }) => {
+  const _onClickUserHandler = ({ target }) => {
     const userId = target.dataset.id;
-
-    const user = await _store.setUser(userId);
-    $userTitle.dataset.username = user.name;
-    $userTitle.innerHTML = `<span><strong>${user.name}</strong>'s Todo List</span>`;
-    setState();
+    selectUser(userId);
   };
 
-  const _listUsers = async () => {
+  const listUsers = (userList) => {
     $userList.querySelector('.user-span').innerHTML = '';
-
-    const userList = await _store.getUsers();
     userList
       .map((user) => _createUserButton(user))
       .forEach((element) => {
@@ -54,20 +46,27 @@ const userList = (setState) => {
     return $button;
   };
 
-  const init = () => {
-    _listUsers();
-
-    $userList
-      .querySelector('.user-create-button')
-      .addEventListener('click', _onUserCreateHandler);
-
-    $userList
-      .querySelector('.user-delete-button')
-      .addEventListener('click', _onUserDeleteHandler);
+  const setTitleName = (name) => {
+    if (!name) {
+      delete $userTitle.dataset.username;
+      $userTitle.innerHTML = 'Todo List App';
+      return;
+    }
+    $userTitle.dataset.username = name;
+    $userTitle.innerHTML = `<span><strong>${name}</strong>'s Todo List</span>`;
   };
 
+  $userList
+    .querySelector('.user-create-button')
+    .addEventListener('click', _onUserCreateHandler);
+
+  $userList
+    .querySelector('.user-delete-button')
+    .addEventListener('click', _onUserDeleteHandler);
+
   return {
-    init,
+    listUsers,
+    setTitleName,
   };
 };
 
