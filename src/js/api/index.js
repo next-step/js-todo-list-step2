ecbb0f72175053c0f0940da3b64af39eb64d49f0
moonee <hellomooneekim@gmail.com>
2021-04-15 21:15:25 +0900

    feat : api 작성

---
+++ src/js/api/index.js
@@ -0,0 +1,143 @@
+const METHODS = {
+  GET: 'GET',
+  POST: 'POST',
+  DELETE: 'DELETE',
+  PUT: 'PUT',
+};
+
+const baseUrl = 'https://js-todo-list-9ca3a.df.r.appspot.com';
+
+const urls = {
+  USER: '/api/users/',
+  USER_TODO: (userId) => `/api/users/${userId}/items/`,
+  UPDATE_TODO: (userId, itemId) => `/api/users/${userId}/items/${itemId}`,
+  TOGGLE_TODO: (userId, itemId) =>
+    `/api/users/${userId}/items/${itemId}/toggle`,
+  SET_PRIORITY: (userId, itemId) =>
+    `/api/users/${userId}/items/${itemId}/priority`,
+};
+
+const errorResponse = (message) => {
+  return {
+    isError: true,
+    errorMessage: message || '잠시 후 다시 시도해주세요',
+  };
+};
+
+const request = async (url, body) => {
+  try {
+    const response = await fetch(baseUrl + url, body);
+    const data = await response.json();
+    if (response.status === 500) {
+      console.log(response);
+      throw { message: data.message };
+    }
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const api = {
+  getUserList: async () => {
+    try {
+      const data = await request(urls.USER, { method: METHODS.GET });
+      return {
+        isError: false,
+        data,
+      };
+    } catch ({ message }) {
+      return errorResponse(message);
+    }
+  },
+
+  getUser: async (userId) => {
+    try {
+      const data = await request(urls.USER + userId, { method: METHODS.GET });
+      return { isError: false, data };
+    } catch ({ message }) {
+      return errorResponse(message);
+    }
+  },
+
+  addUser: async (name) => {
+    try {
+      const data = await request(urls.USER, { method: METHODS.POST, name });
+      return { isError: false, data };
+    } catch ({ message }) {
+      return errorResponse(message);
+    }
+  },
+
+  removeUser: async (userId) => {
+    try {
+      await request(urls.USER + userId, { method: METHODS.DELETE });
+      return { isError: false };
+    } catch ({ message }) {
+      return errorResponse(message);
+    }
+  },
+
+  addTodoItem: async (userId, item) => {
+    try {
+      const data = await request(USER_TODO(userId), {
+        method: METHODS.POST,
+        contents: item,
+      });
+      return { isError: false, data };
+    } catch ({ message }) {
+      return errorResponse(message);
+    }
+  },
+
+  updateTodo: async (userId, itemId) => {
+    try {
+      const data = await request(url.UPDATE_TODO(userId, itemId), {
+        method: METHODS.PUT,
+      });
+      return { isError: false, data };
+    } catch ({ message }) {
+      return errorResponse(message);
+    }
+  },
+  removeTodo: async (userId, itemId) => {
+    try {
+      const data = await request(urls.UPDATE_TODO(userId, itemId), {
+        method: METHODS.DELETE,
+      });
+      return { isError: false, data };
+    } catch ({ message }) {
+      return errorResponse(message);
+    }
+  },
+  removeAllTodos: async (userId) => {
+    try {
+      const data = await request(urls.USER_TODO(userId), {
+        method: METHODS.DELETE,
+      });
+      return { isError: false, data };
+    } catch ({ message }) {
+      return errorResponse(message);
+    }
+  },
+  setTodoPriority: async (userId, itemId, priority) => {
+    try {
+      const data = await request(urls.SET_PRIORITY(userId, itemId), {
+        method: METHODS.PUT,
+        priority,
+      });
+    } catch ({ message }) {
+      return errorResponse(message);
+    }
+  },
+  toggleTodoComplete: async (userId, itemId) => {
+    try {
+      const data = await request(user.TOGGLE_TODO(userId, itemId));
+      return { isError: false, data };
+    } catch ({ message }) {
+      return errorResponse(message);
+    }
+  },
+};
+
+export default api;
