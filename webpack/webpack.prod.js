b2dcaa7db6c9a337549de14cfe61377870512815
이승택 <seungdeng17@gmail.com>
2021-04-16 15:16:54 +0900

    chore: 불필요한 주석 제거

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -3,7 +3,6 @@ const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const Dotenv = require("dotenv-webpack");
 
-// webpack 공통 설정 merge
 module.exports = merge(common, {
   mode: "production",
 
