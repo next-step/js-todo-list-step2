7dc73d97acd739bdea6283f35354c8811cd079b4
sanam <simian114@gmail.com>
2021-04-17 12:41:24 +0900

    feat: priority 라벨 추가

--- src/javascript/view/todoListView.js
+++ src/javascript/view/todoListView.js
@@ -129,7 +129,14 @@ export default class TodoListView {
                     <div class="view">
                       <input class="toggle" type="checkbox"
                         ${todo.isCompleted ? 'checked' : ''}/>
-                      <label class="label">${todo.contents}</label>
+                      <label class="label">
+                      <select class="chip select">
+                        <option value="0" selected>순위</option>
+                        <option value="1">1순위</option>
+                        <option value="2">2순위</option>
+                      </select>
+                      ${todo.contents}
+                      </label>
                       <button class="destroy"></button>
                     </div>
                     <input class="edit" value="" />`;
