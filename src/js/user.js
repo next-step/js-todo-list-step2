d5c9fb4b86602c8d5689112a1fe9ce364b81f033
loverman85 <loverman85@hanmail.net>
2021-04-18 11:28:11 +0900

    refactor: alert메세지 상수적용

--- src/js/user.js
+++ src/js/user.js
@@ -1,16 +1,24 @@
 import {$, utils} from './util.js'
 import {userStore} from './store.js'
+import {Message as msg} from './constant.js'
 
 const onUserCreateHandler = () => {
-  const userName = prompt("추가하고 싶은 이름을 입력해주세요.");
+  const userName = prompt(msg.INPUT_USER_NAME_FOR_ADD);
+  if(!utils.isValid2MoreWord(userName)) {
+    alert(msg.MUST_2_MORE_WORD)
+    return
+  }
+  user().addUser(userName);
+
 }
 
 const userCreateButton = $('.user-create-button')
 userCreateButton.addEventListener('click', onUserCreateHandler)
 
 export const user = () => {
-  const addUser = () => {
+  const addUser = (userName) => {
     console.log('add user')
+    userStore().addUser(userName);
   }
   return { addUser }
 }
