d951cbf48a726954abd612fa5280e928989b6bb8
moonee <hellomooneekim@gmail.com>
2021-04-19 13:31:15 +0900

    fix : 초기 데이터 로딩 에러 처리

--- src/js/App.js
+++ src/js/App.js
@@ -7,26 +7,32 @@ import TodoStore from './libs/TodoStore.js';
 import UserStore from './libs/UserStore.js';
 import api from './api/index.js';
 import UserList from './components/UserList.js';
+import { ERROR_HANDLER } from './utils/errors.js';
 
 const App = () => {
   const init = async () => {
-    const initialData = await api.getUserList();
-    const todoStore = new TodoStore(
-      initialData[0] ? initialData[0]._id : '',
-      initialData[0] ? initialData[0].todoList : [],
-    );
-    const userStore = new UserStore(initialData, todoStore);
-    const todoList = new TodoList(todoStore);
-    const todoCounter = new TodoCounter(todoStore);
-    new TodoInput(todoStore);
-    new TodoFilters(todoStore);
+    try {
+      const initialData = await api.getUserList();
+      const todoStore = new TodoStore(
+        initialData[0] ? initialData[0]._id : '',
+        initialData[0] ? initialData[0].todoList : [],
+      );
+      const userStore = new UserStore(initialData, todoStore);
+      const todoList = new TodoList(todoStore);
+      const todoCounter = new TodoCounter(todoStore);
+      new TodoInput(todoStore);
+      new TodoFilters(todoStore);
 
-    const userTitle = new UserTitle(userStore);
-    const userList = new UserList(userStore);
-    todoStore.addObserver(todoList);
-    todoStore.addObserver(todoCounter);
-    userStore.addObserver(userTitle);
-    userStore.addObserver(userList);
+      const userTitle = new UserTitle(userStore);
+      const userList = new UserList(userStore);
+      todoStore.addObserver(todoList);
+      todoStore.addObserver(todoCounter);
+      userStore.addObserver(userTitle);
+      userStore.addObserver(userList);
+    } catch (error) {
+      const handler = ERROR_HANDLER[error];
+      handler && handler();
+    }
   };
   init();
 };
