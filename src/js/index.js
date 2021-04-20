f77432b29ef0688ff017a3431f9d2632519b4a79
jung9030 <wjddlqkqh57@naver.com>
2021-04-20 17:04:44 +0900

    FEAT : 요구사항, 제약사항 구현

--- src/js/index.js
+++ src/js/index.js
@@ -2,16 +2,19 @@ window.onload = () => {
   getUserList();
 }
 
-const apiUrl = 'https://js-todo-list-9ca3a.df.r.appspot.com';
-const userList = document.querySelector('#user-list');
-const todoList = document.querySelector('.todo-list')
-const newTodoInput = document.querySelector('.new-todo');
-const totalCount = document
-.querySelector('.todo-count')
-.getElementsByTagName('strong')[0];
-const filters = document.querySelector('.filters');
-const clearCompletedButton = document.querySelector('.clear-completed');
+const $ = (selector) => document.querySelector(selector)
 
+const apiUrl = 'https://js-todo-list-9ca3a.df.r.appspot.com';
+const userList = $('#user-list');
+const todoList = $('.todo-list');
+const newTodoInput = $('.new-todo');
+const userName = $('#user-title')
+.getElementsByTagName('span')[0]
+.getElementsByTagName('strong')[0];
+const totalCount = $('.todo-count')
+.getElementsByTagName('strong')[0];
+const filters = $('.filters');
+const clearCompletedButton = $('.clear-completed');
 
 
 function getUserList() {
@@ -25,13 +28,14 @@ function addUserList(data) {
   userList.innerHTML=``;
 
   for (const user of data) {
-    userList.innerHTML += `<button class="ripple" onclick="selectUser('${user._id}')" data-id="${user._id}">${user.name}</button>`;
+    userList.innerHTML += `<button class="ripple" onclick="selectUser('${user._id}', '${user.name}')" data-id="${user._id}" data-name="${user.name}">${user.name}</button>`;
   }
   userList.innerHTML += `<button class="ripple user-create-button" data-action="createUser" onclick="createUser()">  + 유저 생성</button>`;
   userList.innerHTML += `<button class="ripple user-delete-button" data-action="deleteUser" onclick="deleteUser()">  삭제 -</button>`;
 
   // 삭제 시 default
   getUserItems(data[0]._id);
+  userName.innerHTML = data[0].name;
   userList.children[0].classList.add('active');
 }
 
@@ -39,6 +43,10 @@ function addUserList(data) {
 function createUser() {
 
   const userName = prompt("추가하고 싶은 이름을 입력해주세요.");
+  if(userName.length < 2 ){
+    alert('사용자의 이름은 두글자 이상이여야 합니다.');
+    return;
+  }
 
   fetch(apiUrl + '/api/users', {
     method: 'POST',
@@ -54,8 +62,8 @@ function createUser() {
 
 }
 
+function selectUser(id, name) {
 
-function selectUser(id) {
   for(let i = 0; i < userList.children.length; i++) {
     if(userList.children[i].getAttribute('data-id') === id){
       userList.children[i].classList.add('active');
@@ -63,6 +71,8 @@ function selectUser(id) {
       userList.children[i].classList.remove('active');
     }
   }
+
+  userName.innerHTML = name;
 
   getUserItems(id);
 
@@ -100,7 +110,7 @@ function createUserItems(data) {
     }else if(todo.priority === 'SECOND') {
       html += `<span class="chip secondary">2순위</span>`;
     }else {
-      html += `<select class="chip select">`;
+      html += `<select class="chip select" onchange="chipSelectChange('${todo._id}', this.value)">`;
       html += `<option value="0" selected>순위</option>`;
       html += `<option value="1">1순위</option>`;
       html += `<option value="2">2순위</option>`;
@@ -123,9 +133,10 @@ function createUserItems(data) {
 
 
 function deleteUser() {
-  let id = findCurrentUser();
+  let id = findCurrentUserId();
+  let name = findCurrentUserName();
 
-  if(!confirm(id + '을 삭제하시겠습니까?')){
+  if(!confirm(name + '을 삭제하시겠습니까?')){
     return;
   }
 
@@ -137,7 +148,7 @@ function deleteUser() {
 }
 
 
-function findCurrentUser() {
+function findCurrentUserId() {
   let id = "";
 
   for(let i = 0; i < userList.children.length; i++) {
@@ -148,16 +159,107 @@ function findCurrentUser() {
   }
 
   return id;
+}
+
+
+function findCurrentUserName() {
+  let name = "";
+
+  for(let i = 0; i < userList.children.length; i++) {
+    if(userList.children[i].classList.value === 'ripple active'){
+      name = userList.children[i].getAttribute('data-name');
+      break;
+    }
+  }
+
+  return name;
+}
+
+
+function removeTodoList(itemId) {
+  const id = findCurrentUserId();
+
+  fetch(apiUrl + '/api/users/' + id + '/items/' + itemId, {
+    method: 'DELETE'
+  })
+  .then((response) => response.json())
+  .then((json) => getUserItems(id));
+}
+
+
+function toggleTodoList(itemId) {
+  const id = findCurrentUserId();
+
+  fetch(apiUrl + '/api/users/' + id + '/items/' + itemId + '/toggle', {
+    method: 'PUT'
+  })
+  .then((response) => response.json())
+  .then((json) => getUserItems(id));
+}
+
+
+function updateUserItem(e) {
+  const id = findCurrentUserId();
+  const itemId = e.target.getAttribute('data-id');
+  const itemContents = e.target.value;
+
+  fetch(apiUrl + '/api/users/' + id + '/items/' + itemId, {
+    method: 'PUT',
+    body: JSON.stringify({
+      contents: itemContents
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    }
+  })
+  .then((response) => response.json())
+  .then((json) => getUserItems(id));
 
 }
 
 
-newTodoInput.addEventListener('keydown', function(e) {
+
+
+
+function totalCounting() {
+  totalCount.innerHTML = todoList.getElementsByTagName('li').length;
+}
+
+
+function chipSelectChange(itemId, chipValue) {
+  const id = findCurrentUserId();
+
+  const statusList = {
+    0: 'NONE',
+    1: 'FIRST',
+    2: 'SECOND'
+  };
+
+  fetch(apiUrl + '/api/users/' + id + '/items/' + itemId + '/priority', {
+    method: 'PUT',
+    body: JSON.stringify({
+      priority: statusList[chipValue]
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    }
+  })
+  .then((response) => response.json())
+  .then((json) => getUserItems(id));
+}
+
+
+// 입력폼에서 입력시 이벤트
+newTodoInput.addEventListener('keyup', function(e) {
   if(e.key !== 'Enter' || newTodoInput.value === '' ){
     return;
   }
+  if(newTodoInput.value.length < 2 ){
+    alert('내용은 두글자 이상이여야 합니다.');
+    return;
+  }
 
-  let id = findCurrentUser();
+  let id = findCurrentUserId();
 
   console.log(apiUrl + '/api/users/' + id + '/items/');
 
@@ -178,6 +280,7 @@ newTodoInput.addEventListener('keydown', function(e) {
 });
 
 
+// TodoList Item 클릭시 이벤트
 todoList.addEventListener('click', function(e) {
   const {className} = e.target;
   const itemId = e.target.getAttribute('data-id');
@@ -190,31 +293,11 @@ todoList.addEventListener('click', function(e) {
           toggleTodoList(itemId);
           break;
   }
+
 });
 
 
-function removeTodoList(itemId) {
-  const id = findCurrentUser();
-
-  fetch(apiUrl + '/api/users/' + id + '/items/' + itemId, {
-    method: 'DELETE'
-  })
-  .then((response) => response.json())
-  .then((json) => getUserItems(id));
-}
-
-
-function toggleTodoList(itemId) {
-  const id = findCurrentUser();
-
-  fetch(apiUrl + '/api/users/' + id + '/items/' + itemId + '/toggle', {
-    method: 'PUT'
-  })
-  .then((response) => response.json())
-  .then((json) => getUserItems(id));
-}
-
-
+// TodoList Item 더블 클릭시 이벤트
 todoList.addEventListener('dblclick', function(e) {
   const editedParentNode = e.target.parentNode;
   const {parentNode} = editedParentNode;
@@ -225,6 +308,7 @@ todoList.addEventListener('dblclick', function(e) {
 });
 
 
+// TodoList 수정폼에서 입력시 이벤트
 todoList.addEventListener('keyup', function(e) {
   const {parentNode} = e.target;
   const contents = e.target.closest('li').querySelector('label').getAttribute('data-contents');
@@ -238,28 +322,11 @@ todoList.addEventListener('keyup', function(e) {
           updateUserItem(e);
           break;
     }
+
 });
 
-function updateUserItem(e) {
-  const id = findCurrentUser();
-  const itemId = e.target.getAttribute('data-id');
-  const itemContents = e.target.value;
 
-  fetch(apiUrl + '/api/users/' + id + '/items/' + itemId, {
-    method: 'PUT',
-    body: JSON.stringify({
-      contents: itemContents
-    }),
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    }
-  })
-  .then((response) => response.json())
-  .then((json) => getUserItems(id));
-
-}
-
-
+// 필터 클릭시 이벤트
 filters.addEventListener('click', function(e) {
 
   // 모든 filter 선택해제 -> 선택한 filter selected 추가
@@ -306,17 +373,17 @@ filters.addEventListener('click', function(e) {
     }
 });
 
-function totalCounting() {
-  totalCount.innerHTML = todoList.getElementsByTagName('li').length;
-}
 
-
+// '모두 삭제' 버튼 클릭시 이벤트
 clearCompletedButton.addEventListener('click', function(e) {
-  const id = findCurrentUser();
+  const id = findCurrentUserId();
+
+  console.log(apiUrl + '/api/users/' + id + '/items/');
 
   fetch(apiUrl + '/api/users/' + id + '/items/', {
     method: 'DELETE'
   })
-    .then(getUserList())
+  .then((response) => response.json())
+  .then((json) => getUserItems(id));
 
 });
