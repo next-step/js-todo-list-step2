454a198f7f9431a7722d52857caec78063a444dd
HJ-Woo <hjwoou@gmail.com>
2021-04-26 17:35:05 +0900

    fix: 전체보기/해야할일/완료한일 클릭시 selected 활성화

--- src/js/components/todo/todoCount.js
+++ src/js/components/todo/todoCount.js
@@ -1,22 +1,34 @@
-import { checkClassName, checkLocalName, getClassName } from "../../utils/eventUtils.js";
+import {
+  checkClassName,
+  checkLocalName,
+  getClassName,
+  removeSelect,
+  setSelect,
+} from "../../utils/eventUtils.js";
 
 export default function TodoCount(app) {
   const todoCount = document.querySelector(".count-container");
-  
-  this.render = items => {
-    todoCount.querySelector("strong").textContent = items.length;
-  }
 
-  const onClickHandler = event => {
-    if(checkLocalName(event, "a")) {
+  this.render = (items) => {
+    todoCount.querySelector("strong").textContent = items.length;
+  };
+
+  const select = (event) => {
+    todoCount.querySelectorAll("a").forEach((each) => removeSelect(each));
+    setSelect(event);
+  };
+
+  const onClickHandler = (event) => {
+    if (checkLocalName(event, "a")) {
+      select(event);
       document.location.hash = getClassName(event);
       app.render();
       return;
     }
-    if(checkClassName(event, "clear-completed")) {
+    if (checkClassName(event, "clear-completed")) {
       app.deleteAll();
     }
-  }
+  };
 
   todoCount.addEventListener("click", onClickHandler);
 }
--- src/js/utils/eventUtils.js
+++ src/js/utils/eventUtils.js
@@ -3,12 +3,17 @@ import { checkEmpty } from "./stringUtils.js";
 export const checkClassName = (event, name) => event.target.className === name;
 export const checkLocalName = (event, name) => event.target.localName === name;
 export const checkKey = (event, key) => event.key === key;
-export const isEmptyValue = event => checkEmpty(getValue(event));
+export const isEmptyValue = (event) => checkEmpty(getValue(event));
 
-export const getClassName = event => event.target.className;
-export const getValue = event => event.target.value;
-export const getClassLiId = event => event.target.closest("li").getAttribute("data-id");
-export const getButtonId = event => event.target.closest("button").getAttribute("data-id");
+export const getClassName = (event) => event.target.className;
+export const getValue = (event) => event.target.value;
+export const getClassLiId = (event) =>
+  event.target.closest("li").getAttribute("data-id");
+export const getButtonId = (event) =>
+  event.target.closest("button").getAttribute("data-id");
+export const setSelect = (event) => event.target.classList.add("selected");
+export const removeSelect = (event) => event.classList.remove("selected");
 
-export const isUserTarget = event => event.target.closest("user-list-item") !== null && event.target.getAttribute("data-action") === "selectUser";
-
+export const isUserTarget = (event) =>
+  event.target.closest("user-list-item") !== null &&
+  event.target.getAttribute("data-action") === "selectUser";
