37e9a719e211d6d57856d7462cf6daa33f324cb0
Park Gyu Ae <yunos0502@gmail.com>
2021-04-26 13:46:45 +0900

    feat/fix:user 삭제 기능 추가/ user 추가 기능 수정

--- src/js/components/Users/UserList.js
+++ src/js/components/Users/UserList.js
@@ -1,33 +1,40 @@
 import { $ } from '../../utils/common.js';
+import { onUserHandler, rippleActiveHandler } from '../../utils/event.js';
 
 export default function UserList() {
   this.$Users = [];
   this.$UsersDom = $('#user-list');
 
-  this.setState = (users) => {
+  this.setState = (users = []) => {
     this.$Users = users;
     this.render();
   }
 
   this.render = () => {
-    const users = this.$Users.map(user => template(user));
+    const users = Array.from(this.$Users).map(user => template(user));
     this.$UsersDom.innerHTML += users.join('');
 
+    const userButtons = document.createElement('div');
+    userButtons.style.display = 'inline-block';
+    
     const userCreateButton = document.createElement('button');
     userCreateButton.classList.add('ripple', 'user-create-button');
     userCreateButton.textContent = '+ 유저 생성';
+    userButtons.appendChild(userCreateButton);
 
     const userDeleteButton = document.createElement('button');
     userDeleteButton.classList.add('ripple', 'user-delete-button');
     userDeleteButton.textContent = '삭제 -';
+    userButtons.appendChild(userDeleteButton);
 
-    this.$UsersDom.appendChild(userCreateButton);
-    this.$UsersDom.appendChild(userDeleteButton);
+    this.$UsersDom.appendChild(userButtons);
+    this.$UsersDom.addEventListener('click', onUserHandler);
+    rippleActiveHandler(this.$UsersDom.firstChild);
   }
 }
 
 function template(user) {
-  const userTemplate = `<button class="ripple" data-_id="${user.id}">${user.name}</button>`;
+  const userTemplate = `<button class="ripple" data-_id="${user._id}">${user.name}</button>`;
 
   return userTemplate;
 }
---
+++ src/js/utils/event.js
@@ -0,0 +1,56 @@
+import API from '../api/index.js';
+import Todos from '../components/Todos/index.js';
+import Users from '../components/Users/index.js';
+import UserList from '../components/Users/UserList.js';
+import { $, $All } from './common.js';
+
+export const onUserHandler = (e) => {
+  if (!e.target.matches('button')) return;
+
+  (e.target.matches('.user-create-button')) && onUserCreateHandler();
+  (e.target.matches('.user-delete-button')) && onUserDeleteHandler();
+  (e.target.dataset._id) && onUserTodos(e);
+}
+
+const onUserCreateHandler = () => {
+  const userName = prompt("추가하고 싶은 이름을 입력해주세요.");
+  const user = addUser(userName);
+
+  return user;
+}
+
+async function addUser(userName) {
+  const createUser = await API.addUser(userName);
+  const userTemplate = `<button class="ripple" data-_id="${createUser._id}">${createUser.name}</button>`;
+  
+  $('#user-list').insertAdjacentHTML('afterbegin', userTemplate);
+  rippleActiveHandler($('#user-list').firstChild);
+
+  return userTemplate;
+}
+
+const onUserDeleteHandler = () => {
+  const userName = confirm("정말 삭제하시겠습니까?");
+
+  if (!userName) return;
+
+  const userList = $All('[data-_id]');
+  const user = Array.from(userList).find(user => user.className.includes('active'));
+  const userId = user.dataset._id;
+  API.deleteUser(userId);
+  user.remove();
+}
+
+const onUserTodos = (e) => {
+  rippleActiveHandler(e.target);
+
+  const userTodoList = API.getUserTodoList(e.target.dataset._id);
+  Todos(userTodoList);
+}
+
+export function rippleActiveHandler(ripple) {
+  const $ripples = $All('.ripple');
+
+  Array.from($ripples).map(ripple => ripple.classList.remove('active'));
+  ripple.classList.add('active');
+}
