20cea6433aa7df667268e29bfbf50bc2c5f42f01
Park Gyu Ae <yunos0502@gmail.com>
2021-04-26 14:00:20 +0900

    feat: user 이름이 최소 2글자 이상 시 등록되도록 추가

--- src/js/utils/event.js
+++ src/js/utils/event.js
@@ -16,9 +16,13 @@ const onUserCreateHandler = () => {
   const userName = prompt("추가하고 싶은 이름을 입력해주세요.");
 
   if (!userName) return;
+  if (userName.length < 2) {
+    alert('이름은 최소 2글자 이상이어야 합니다.');
+    return;
+  }
 
   const user = addUser(userName);
-  
+
   return user;
 }
 
