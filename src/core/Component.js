f09f9026ebf8ed8c139fe5cfd877ecbe0fd65775
Jay <53712717+EungyuCho@users.noreply.github.com>
2021-06-01 15:42:43 +0900

    feat: Component(core)

---
+++ src/core/Component.js
@@ -0,0 +1,27 @@
+export class Component {
+  $target
+  $props = {}
+  $state = {}
+  $render
+
+  constructor(target, props = {}, state = {}) {
+    this.$target = target
+    this.$props = props
+    this.$state = state
+
+    this.$render = () => {
+      target.innerHTML = this.render()
+    }
+
+    this.setState(state)
+    this.setEvent(target)
+  }
+
+  setState(state) {
+    this.$state = { ...this.$state, ...state }
+  }
+
+  setEvent(target) {}
+
+  render() {}
+}
