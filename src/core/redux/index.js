b79db3b2dc1d30e27553db80636c7cd9b7b3f65f
Jay <53712717+EungyuCho@users.noreply.github.com>
2021-06-02 18:05:11 +0900

    remove: :coffin: remove daed code

--- src/core/redux/index.js
+++ src/core/redux/index.js
@@ -1,5 +1,4 @@
 import createStore from './createStore.js'
 import createAction from './createAction.js'
-import combineReducers from './combineReducers.js'
 
-export { createStore, createAction, combineReducers }
+export { createStore, createAction }
