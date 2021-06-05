2acebe827ca2b8559dc67203802b675dd8b828b4
Joy <hyewon3938@naver.com>
2021-06-05 14:41:21 +0900

    feat : 서버에서 userList 받아오기 구현
    - 서버에서 받은 데이터로 userList 렌더링 기능 구현

--- src/js/components/TodoApp.js
+++ src/js/components/TodoApp.js
@@ -1,6 +1,8 @@
 import UserList from "./UserList.js";
 import { $ } from "../lib/util.js";
+import { fetchRequest } from "../lib/fetchRequest.js";
 import UserModel from "../model/UserModel.js";
+import { API_URL } from "../constants/config.js";
 
 class TodoApp {
   constructor() {
@@ -9,7 +11,16 @@ class TodoApp {
     this.init();
   }
 
-  init() {}
+  async init() {
+    const { result } = await fetchRequest(API_URL.USER, "get");
+
+    const userListData = result.map((user) => {
+      return new UserModel({ id: user._id, name: user.name, todoList: user.todoList });
+    });
+
+    this.users = userListData;
+    this.userList.setState(this.users);
+  }
 
   onAddUser() {
     const userName = prompt("추가하고 싶은 이름을 입력해주세요.");
---
+++ src/js/constants/config.js
@@ -0,0 +1,7 @@
+const baseUrl = "https://js-todo-list-9ca3a.df.r.appspot.com";
+
+const API_URL = {
+  USER: `${baseUrl}/api/users`,
+};
+
+export { API_URL };
