5c0f5f747086af307d5b9545d9a624b944a5d265
woochul2 <leewoo0686@gmail.com>
2021-06-06 14:03:14 +0900

    feat: user 선택 기능 구현

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,13 @@
 const $userList = document.querySelector('#user-list');
+const $userName = document.querySelector('#user-title strong');
 
 const API_URL = 'https://js-todo-list-9ca3a.df.r.appspot.com/api/users';
 
+let users = [];
+let activeUser = { _id: '', name: '' };
+
 function getUserTemplate({ _id, name }) {
-  return `<button class="ripple" id=${_id}>${name}</button>`;
+  return `<button class="ripple ${_id === activeUser._id && 'active'}" id=${_id}>${name}</button>`;
 }
 
 const userControlButtonTemplate = `
@@ -15,18 +19,34 @@ const userControlButtonTemplate = `
   </button>
 `;
 
-function getUsers() {
-  return new Promise((resolve) => {
-    fetch(API_URL).then((response) => {
-      resolve(response.json());
-    });
-  });
+async function getUsers() {
+  const response = await fetch(API_URL);
+  return response.json();
 }
 
-async function main() {
-  const users = await getUsers();
+function render() {
   const usersTemplate = users.map(getUserTemplate);
   $userList.innerHTML = usersTemplate.join('') + userControlButtonTemplate;
+  $userName.innerText = activeUser.name;
+}
+
+async function loadUsers() {
+  users = await getUsers();
+  activeUser = users[0];
+  render();
+}
+
+function selectUser(event) {
+  const userTarget = event.target;
+  if (!userTarget.classList.contains('ripple') || userTarget.id === '') return;
+  activeUser = { _id: userTarget.id, name: userTarget.innerText };
+  render();
+}
+
+function main() {
+  loadUsers();
+
+  $userList.addEventListener('click', selectUser);
 }
 
 main();
