ded8b8cdc193639830e7f5126f2e91058f9b3e3d
thdwlsgus0 <thdwlsgus0@nts-corp.com>
2021-06-07 01:48:30 +0900

    feat: 2글자 미만이면 경고창 띄우기

--- src/event/AddHandler.js
+++ src/event/AddHandler.js
@@ -4,6 +4,9 @@ import { timer } from '../utils/timer.js';
 export const setUserHandler = async (e) => {
   let nickname = prompt('추가하고 싶은 이름을 입력하세요');
   if (nickname === '') return;
+  if (nickname.length < 2) {
+    alert('user의 이름은 최소 2글자 이상이어야 합니다.');
+  }
   createUser(nickname);
   timer(render, 2000);
 };
