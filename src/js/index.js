99978269c57bf3ba27f4b55c8a54124ea040b65f
woochul2 <leewoo0686@gmail.com>
2021-06-06 14:45:01 +0900

    feat: user 추가 기능 구현

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 const $userList = document.querySelector('#user-list');
-const $userName = document.querySelector('#user-title strong');
+const $username = document.querySelector('#user-title strong');
 
 const API_URL = 'https://js-todo-list-9ca3a.df.r.appspot.com/api/users';
 
@@ -19,19 +19,34 @@ const userControlButtonTemplate = `
   </button>
 `;
 
-async function getUsers() {
+async function getUsersData() {
   const response = await fetch(API_URL);
   return response.json();
+}
+
+async function addUserData(username) {
+  const data = { name: username };
+  await fetch(API_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
 }
 
 function render() {
   const usersTemplate = users.map(getUserTemplate);
   $userList.innerHTML = usersTemplate.join('') + userControlButtonTemplate;
-  $userName.innerText = activeUser.name;
+  $username.innerText = activeUser.name;
 }
 
 async function loadUsers() {
-  users = await getUsers();
+  users = await getUsersData();
+}
+
+async function initUsers() {
+  await loadUsers();
   activeUser = users[0];
   render();
 }
@@ -43,10 +58,24 @@ function selectUser(event) {
   render();
 }
 
+async function addUser(event) {
+  const addUserButtonTarget = event.target;
+  if (!addUserButtonTarget.classList.contains('user-create-button')) return;
+  const username = prompt('추가하고 싶은 이름을 입력해주세요.');
+  if (username.length < 2) {
+    window.alert('2글자 이상이어야 합니다.');
+    return;
+  }
+  await addUserData(username);
+  await loadUsers();
+  render();
+}
+
 function main() {
-  loadUsers();
+  initUsers();
 
   $userList.addEventListener('click', selectUser);
+  $userList.addEventListener('click', addUser);
 }
 
 main();
