a1b5641a236a1eda6c1a230bc1cb15cce7ce36ad
kmkim <kkgg7527@gmail.com>
2021-06-07 00:46:08 +0900

    feat: todo item complete

--- README.md
+++ README.md
@@ -28,7 +28,7 @@
 ## 🎯 요구사항
 
 - [x] 1. User 추가하기
-- [ ] 2. User의 투두리스트 불러오기
+- [x] 2. User의 투두리스트 불러오기
 - [x] 3. User 삭제하기
 - [x] 3. todoItem 추가하기
 - [x] 4. todoItem 불러오기
@@ -40,15 +40,15 @@
 
 ## 🎯🎯 심화 요구사항
 
-- [ ] 1. fetch api 사용하는 부분을 async await을 사용하여 리팩토링하기.
+- [x] 1. fetch api 사용하는 부분을 async await을 사용하여 리팩토링하기.
 - [ ] 2. github issue에서 라벨을 붙이는 것처럼, 우선순위에 따라서 label를 추가하기.
-- [ ] 3. ES6 impot & export를 이용해 자바스크립트 파일을 리팩토링하기.
+- [x] 3. ES6 impot & export를 이용해 자바스크립트 파일을 리팩토링하기.
 
 <br/>
 
 ## 🕵️‍♂️ 제약사항
 
-- [ ] 1. User의 이름은 최소 2글자 이상이어야 한다.
+- [x] 1. User의 이름은 최소 2글자 이상이어야 한다.
 - [x] 2. TodoItem Contents는 최소 2글자 이상이어야 한다.
 
 <br/>
--- src/js/components/TodoList.js
+++ src/js/components/TodoList.js
@@ -2,11 +2,11 @@ import { $, $$ } from "../utils/querySelector.js";
 import API from "../api/api.js";
 
 export default function TodoList () {
-	const $new = ({ contents, isCompleted }) => {
+	const $new = ({ _id, contents, isCompleted }) => {
 		return `
 			<li class="${isCompleted ? "completed" : "new"}">
 				<div class="view">
-					<input class="toggle" type="checkbox" />
+					<input class="toggle" type="checkbox" data-id="${ _id }"/>
 					<label class="label">
 						<select class="chip select">
 							<option value="0" selected>순위</option>
@@ -25,6 +25,8 @@ export default function TodoList () {
 	this.setState = (todos) => {
 		let items = "";
 
+		console.log("todo" , todos)
+
 		todos.map(todo => items += $new(todo));
 
 		$(".todo-list").innerHTML = items;
@@ -35,12 +37,16 @@ export default function TodoList () {
 	}
 
 	const complete = (target) => {
+		const userId = $(".active").dataset.id;
+		const itemId = target.dataset.id;
+		
 		const chrBool = target.checked;
 		const $parentLi = (target.parentElement).parentElement;
 
 		$parentLi.classList.remove(chrBool ? "new" : "completed" );
 		$parentLi.classList.add(chrBool ? "completed" : "new");
 
+		API.putFetch(`/api/users/${ userId }/items/${ itemId }/toggle`);
 	}
 
 
