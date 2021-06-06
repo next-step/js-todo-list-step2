2179705cfb5db02a0df077affcdcfccc92125ecc
thdwlsgus0 <thdwlsgus0@nts-corp.com>
2021-06-06 14:22:49 +0900

    build: httpstatus 정의

--- constants/HttpMethod.js
+++ constants/HttpMethod.js
@@ -3,5 +3,6 @@ const POST = 'POST';
 const PUT = 'PUT';
 const PATCH = 'PATCH';
 const DELETE = 'DELETE';
+const OPTION = 'OPTION';
 
-export default { GET, POST, PUT, PATCH, DELETE };
+export default { GET, POST, PUT, PATCH, DELETE, OPTION };
